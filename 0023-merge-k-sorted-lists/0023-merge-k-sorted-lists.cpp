e3206ba7c6b9abb270d2975352e06b21ba634cb0
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-02-21 19:52:46 +0530

    Time: 201 ms (19.88%), Space: 13.5 MB (46.15%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1==NULL) return list2;
+        else if(list2==NULL) return list1;
+        else if(list1->val<=list2->val) 
+        {
+            list1->next=mergeTwoLists(list1->next,list2);
+            return list1;
+        }
+        else
+        {
+            list2->next=mergeTwoLists(list1,list2->next);
+            return list2;
+        }
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size()==0) return NULL;
+        ListNode *head=lists[0];
+        for(int i=1;i<lists.size();i++) head=mergeTwoLists(head,lists[i]);
+        return head;
+    }
+};
