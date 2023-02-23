fc3b54183495d2a33d0b507947c4c04e0b3d1c8d
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-02-23 12:45:23 +0530

    Time: 0 ms (100.00%), Space: 11.2 MB (64.54%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
@@ -10,13 +10,9 @@ public:
                 while(curr->next && curr->val==curr->next->val) curr=curr->next;
                 if(!prev) head=curr->next;
                 else prev->next=curr->next;
-                curr=curr->next;
             }
-            else
-            {
-                prev=curr;
-                curr=curr->next;
-            }
+            else prev=curr;
+            curr=curr->next;
         }
         return head;
     }
