028dc087b48325be5018238e679e9a1d6c137511
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-02-27 21:39:04 +0530

    Time: 23 ms (46.25%), Space: 25.5 MB (33.24%) - LeetHub

---
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    unordered_map<TreeNode*,int>mp;
+    int rob(TreeNode* root) {
+        if(!root) return 0;
+        if(mp.find(root)!=mp.end()) return mp[root];
+        int not_take=rob(root->left)+rob(root->right);
+        int take=root->val;
+        if(root->left) take+=rob(root->left->left)+rob(root->left->right);
+        if(root->right) take+=rob(root->right->left)+rob(root->right->right);
+        return mp[root]=max(not_take,take);
+    }
+};
