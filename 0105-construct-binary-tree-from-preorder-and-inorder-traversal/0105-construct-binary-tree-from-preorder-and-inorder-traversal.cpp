3fa53c174215b2d997a1c782204139e40176e46f
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-03-01 23:03:33 +0530

    Time: 48 ms (25.28%), Space: 25.9 MB (82.68%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     TreeNode* build(vector<int>& preorder, vector<int>& inorder, int& rootIdx, int left, int right)     {
-        if (left > right) return NULL;
-        int pivot = left;  // find the root from inorder
-        while(inorder[pivot] != preorder[rootIdx]) pivot++;
+        if(left>right) return NULL;
+        int pivot=left;  
+        while(inorder[pivot]!=preorder[rootIdx]) pivot++;
         rootIdx++;
-        TreeNode* newNode = new TreeNode(inorder[pivot]);
-        newNode->left = build(preorder, inorder, rootIdx, left, pivot-1);
-        newNode->right = build(preorder, inorder, rootIdx, pivot+1, right);
+        TreeNode* newNode=new TreeNode(inorder[pivot]);
+        newNode->left=build(preorder,inorder,rootIdx,left,pivot-1);
+        newNode->right=build(preorder,inorder,rootIdx,pivot+1,right);
         return newNode;
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
