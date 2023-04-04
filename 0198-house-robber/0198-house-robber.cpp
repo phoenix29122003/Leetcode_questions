decc9a8c5459c49bf811b30c00a37e422093c00c
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-04-04 11:31:19 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (9.64%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int solve(vector<int>& nums,int idx,vector<int>&dp)
+    {
+        if(idx>=nums.size()) return 0;
+        if(dp[idx]!=-1) return dp[idx];
+        int take=nums[idx]+solve(nums,idx+2,dp);
+        int nottake=solve(nums,idx+1,dp);
+        return dp[idx]=max(take,nottake);
+    }
+    int rob(vector<int>& nums) {
+        vector<int>dp(nums.size()+1,-1);
+        return solve(nums,0,dp);
+    }
+};
