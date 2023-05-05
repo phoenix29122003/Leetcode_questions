961c3add823f526673a937513656b4be4a485945
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-05-05 20:22:54 +0530

    Time: 37 ms (80.19%), Space: 19 MB (72.59%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        if(intervals.size()<=1) return intervals;
         sort(intervals.begin(),intervals.end());
         vector<vector<int>>ans;
         ans.push_back(intervals[0]);
         for(int i=1;i<intervals.size();i++)
         {
-            if(intervals[i][0]<=ans.back()[1]) 
+            if(intervals[i][0]<=ans.back()[1])
             {
                 ans.back()[1]=max(ans.back()[1],intervals[i][1]);
             }
