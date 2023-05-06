0504021ef7a34739b2fa0647afa03bfeafe03b40
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-05-07 04:53:31 +0530

    Time: 31 ms (95.87%), Space: 28.1 MB (68.96%) - LeetHub

---
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numPairsDivisibleBy60(vector<int>& time) {
+        int map[60]={};
+        int ans=0;
+        for(auto x:time)
+        {
+            int t=x%60;
+            int y=(60-t)%60;
+            ans+=map[y];
+            map[t]++;
+        }
+        return ans;
+    }
+};
