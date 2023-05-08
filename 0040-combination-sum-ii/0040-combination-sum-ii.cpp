a20c5193f8bc60f38006394108750a967b83d6a9
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-05-08 06:08:57 +0530

    Time: 7 ms (60.75%), Space: 10.6 MB (86.56%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
-    
-void solve(vector<int>& num,int target,int idx,vector<int>& ds,vector<vector<int>>& ans)
+    void solve(vector<int>&nums,int target,int idx,vector<int>&ds,vector<vector<int>>&ans)
     {
         if(target==0)
         {
             ans.push_back(ds);
             return;
         }
-        else
+        for(int i=idx;i<nums.size();i++)
         {
-            for(int i=idx;i<num.size();i++) 
-            {
-                if(num[i]>target) return;
-                if(i&&num[i]==num[i-1]&&i>idx) continue; 
-                ds.push_back(num[i]),
-                solve(num,target-num[i],i+1,ds,ans); 
-                ds.pop_back();
-            }
+            if(nums[i]>target) return;
+            if(i && nums[i]==nums[i-1] && i!=idx) continue;
+            ds.push_back(nums[i]);
+            solve(nums,target-nums[i],i+1,ds,ans);
+            ds.pop_back();
         }
     }
-    vector<vector<int>> combinationSum2(vector<int> &num, int target) 
-    {
+    vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
         vector<vector<int>>ans;
         vector<int>ds;
-        sort(num.begin(),num.end());
-        solve(num,target,0,ds,ans);
+        sort(nums.begin(),nums.end());
+        solve(nums,target,0,ds,ans);
         return ans;
     }
 };
