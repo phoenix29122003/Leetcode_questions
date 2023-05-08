346a4f71036397e7a2c6e20dbc822609f8008a36
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-05-08 21:11:47 +0530

    Time: 139 ms (14.74%), Space: 59.5 MB (5.02%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int solve(vector<vector<char>>& matrix,int i,int j,int m,int n,vector<vector<int>>&dp)
+    {
+        if(i<0 || j<0 || i>=m || j>=n || matrix[i][j]=='0') return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        return dp[i][j]=1+min({solve(matrix,i+1,j,m,n,dp),solve(matrix,i+1,j+1,m,n,dp),solve(matrix,i,j+1,m,n,dp)});
+    }
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int m=matrix.size(),n=matrix[0].size(),ans=INT_MIN;
+        vector<vector<int>>dp(301,vector<int>(301,-1));
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(matrix[i][j]=='1') ans=max(ans,solve(matrix,i,j,m,n,dp));
+            }
+        }
+        return ans==INT_MIN?0:ans*ans;
+    }
+};
