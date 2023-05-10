10e65a90c380eba9247b2fda6cb9e913b98fa089
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-05-10 05:53:41 +0530

    Time: 106 ms (9.66%), Space: 26.2 MB (50.58%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -12,12 +12,7 @@ public:
         int m=matrix.size(),n=matrix[0].size(),ans=0;
         vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
         for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                ans+=solve(matrix,i,j,m,n,dp);
-            }
-        }
+            for(int j=0;j<n;j++) ans+=solve(matrix,i,j,m,n,dp);
         return ans;
     }
 };
