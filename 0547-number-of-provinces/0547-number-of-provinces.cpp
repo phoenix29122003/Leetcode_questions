2754fcc3642ce07d491a1c4a3e917655aa3fa49a
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-05-13 16:15:14 +0530

    Time: 16 ms (97.59%), Space: 13.7 MB (75.77%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
-    void solve(vector<vector<int>>& graph,vector<bool>&vis,int src)
+    void solve(vector<vector<int>>& graph,vector<int>&vis,int src)
     {
         vis[src]=true;
-        for(int j=0;j<graph.size();j++) if(graph[src][j] && !vis[j]) solve(graph,vis,j);
+        for(int j=0;j<graph.size();j++) if(!vis[j] && graph[src][j]==1) solve(graph,vis,j);
     }
     int findCircleNum(vector<vector<int>>& graph) {
-        int ans=0,n=graph.size();
-        vector<bool>vis(n,false);
-        for(int i=0;i<n;i++)
-        {
-            if(!vis[i]) ans++,solve(graph,vis,i);
-        }
+        int n=graph.size(),ans=0;
+        vector<int>vis(n,false);
+        for(int i=0;i<n;i++) if(!vis[i]) ans++,solve(graph,vis,i);
         return ans;
     }
 };
