38531639149d5c60e0c2a988e961608912df602e
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-05-13 16:42:16 +0530

    Time: 16 ms (68.65%), Space: 25.4 MB (39.27%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -4,10 +4,10 @@ public:
     int rob(TreeNode* root) {
         if(!root) return 0;
         if(mp.find(root)!=mp.end()) return mp[root];
-        int not_take=rob(root->left)+rob(root->right);
+        int nottake=rob(root->left)+rob(root->right);
         int take=root->val;
         if(root->left) take+=rob(root->left->left)+rob(root->left->right);
         if(root->right) take+=rob(root->right->left)+rob(root->right->right);
-        return mp[root]=max(not_take,take);
+        return mp[root]=max(take,nottake);
     }
 };
