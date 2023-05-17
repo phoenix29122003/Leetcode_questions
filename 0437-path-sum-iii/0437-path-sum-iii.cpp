d36a735a930779db4db4d1939e47f8f1363cf4d8
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-05-17 21:32:24 +0530

    Time: 11 ms (98.58%), Space: 19.1 MB (45.43%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    unordered_map<long,int>mp;
     int ans=0;
+    unordered_map<long,int>mp;
     void solve(TreeNode *root,int target,long sum)
     {
-        if(!root) return;
+        if(root==NULL) return;
         sum+=root->val;
         if(sum==target) ans++;
         if(mp.find(sum-target)!=mp.end()) ans+=mp[sum-target];
