985644d6a2eaa416cf8718a3a8fff4e31e48f8e9
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-05-19 21:42:18 +0530

    Time: 171 ms (14.34%), Space: 45.4 MB (5.48%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -7,14 +7,11 @@ public:
             ans.push_back(nums);
             return;
         }
-        else
+        for(int i=idx;i<nums.size();i++)
         {
-            for(int i=idx;i<nums.size();i++)
-            {
-                swap(nums[idx],nums[i]);
-                solve(nums,idx+1,ans);
-                swap(nums[idx],nums[i]);
-            }
+            swap(nums[idx],nums[i]);
+            solve(nums,idx+1,ans);
+            swap(nums[idx],nums[i]);
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
