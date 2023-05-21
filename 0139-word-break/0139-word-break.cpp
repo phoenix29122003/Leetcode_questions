8f15137704cd8d4e049fcce0dc283823359299f1
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-05-21 11:21:07 +0530

    Time: 24 ms (28.20%), Space: 14.6 MB (21.01%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -7,14 +7,13 @@ public:
         for(int i=idx;i<s.length();i++)
         {
             string str=s.substr(idx,i-idx+1);
-            if(set.find(str)!=set.end() && solve(s,i+1,set,dp)) return dp[idx]=1;
+            if(set.find(str)!=set.end() && solve(s,i+1,set,dp)) return dp[idx]=true;
         }
-        return dp[idx]=0;
+        return dp[idx]=false;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string>set;
-        for(int i=0;i<wordDict.size();i++) set.insert(wordDict[i]);
-        vector<int>dp(1001,-1);
+        unordered_set<string>set(wordDict.begin(),wordDict.end());
+        vector<int>dp(301,-1);
         return solve(s,0,set,dp);
     }
 };
