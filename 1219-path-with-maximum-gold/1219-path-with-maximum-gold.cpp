15891749d2a125fce57a71d111205fb4d2c91e69
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-05-22 14:39:01 +0530

    Time: 329 ms (57.27%), Space: 7.4 MB (94.82%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>>& grid,int i,int j,int m,int n)
+    int solve(vector<vector<int>>&grid,int i,int j,int m,int n)
     {
         if(i<0 || j<0 || i>=m || j>=n || grid[i][j]==0) return 0;
         int cnt=grid[i][j];
         grid[i][j]=0;
-        int ans=cnt+max({dfs(grid,i-1,j,m,n),dfs(grid,i+1,j,m,n),dfs(grid,i,j-1,m,n),dfs(grid,i,j+1,m,n)});
+        int ans=cnt+max({solve(grid,i-1,j,m,n),solve(grid,i+1,j,m,n),solve(grid,i,j-1,m,n),solve(grid,i,j+1,m,n)});
         grid[i][j]=cnt;
         return ans;
     }
@@ -13,7 +13,7 @@ public:
         int m=grid.size(),n=grid[0].size(),ans=0;
         for(int i=0;i<m;i++)
             for(int j=0;j<n;j++)
-                if(grid[i][j]) ans=max(ans,dfs(grid,i,j,m,n));
+                if(grid[i][j]) ans=max(ans,solve(grid,i,j,m,n));
         return ans;
     }
 };
