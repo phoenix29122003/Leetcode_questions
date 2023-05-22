cf09c7cb5bd7cd34d62d1c4db230903444d51f29
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-05-22 11:29:10 +0530

    Time: 624 ms (10.50%), Space: 18 MB (12.13%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int solve(vector<int>& nums,vector<int>&dp,int pos)
+    int solve(vector<int>&nums,int pos,vector<int>&dp)
     {
         if(pos>=nums.size()-1) return 0;
         if(dp[pos]!=10001) return dp[pos];
         int ans=10001;
         for(int i=1;i<=nums[pos];i++)
         {
-            ans=min(ans,1+solve(nums,dp,pos+i));
+            ans=min(ans,1+solve(nums,pos+i,dp));
         }
         return dp[pos]=ans;
     }
     int jump(vector<int>& nums) {
-        vector<int>dp(nums.size(),10001);
-        return solve(nums,dp,0);
+        vector<int>dp(nums.size()+1,10001);
+        return solve(nums,0,dp);
     }
 };
