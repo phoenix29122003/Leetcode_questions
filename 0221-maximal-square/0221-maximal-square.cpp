82599d2556a0d93fcc88a9d72978edbd8c3d00f6
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-05-22 10:29:44 +0530

    Time: 130 ms (16.20%), Space: 21.2 MB (10.22%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-    int solve(vector<vector<char>>& matrix,int i,int j,int m,int n,vector<vector<int>>&dp)
+    int solve(vector<vector<char>>&matrix,int i,int j,int m,int n,vector<vector<int>>&dp)
     {
         if(i<0 || j<0 || i>=m || j>=n || matrix[i][j]=='0') return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        return dp[i][j]=1+min({solve(matrix,i+1,j,m,n,dp),solve(matrix,i+1,j+1,m,n,dp),solve(matrix,i,j+1,m,n,dp)});
+        return dp[i][j]=1+min({solve(matrix,i,j+1,m,n,dp),solve(matrix,i+1,j+1,m,n,dp),solve(matrix,i+1,j,m,n,dp)});
     }
     int maximalSquare(vector<vector<char>>& matrix) {
-        int m=matrix.size(),n=matrix[0].size(),ans=INT_MIN;
-        vector<vector<int>>dp(301,vector<int>(301,-1));
+        int m=matrix.size(),n=matrix[0].size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        int ans=INT_MIN;
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
