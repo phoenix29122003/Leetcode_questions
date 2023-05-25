09f6bd98e52c5c74e4c7e6b44ef8bd7829923275
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-05-25 17:20:09 +0530

    Time: 59 ms (20.68%), Space: 81.5 MB (9.03%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -6,10 +6,7 @@ public:
         if(n==0) return dp[m][n]=m;
         if(dp[m][n]!=-1) return dp[m][n];
         if(w1[m-1]==w2[n-1]) return dp[m][n]=solve(w1,w2,m-1,n-1,dp);
-        else
-        {
-            return dp[m][n]=1+min({solve(w1,w2,m-1,n-1,dp),solve(w1,w2,m-1,n,dp),solve(w1,w2,m,n-1,dp)});
-        }
+        else return dp[m][n]=1+min({solve(w1,w2,m-1,n-1,dp),solve(w1,w2,m-1,n,dp),solve(w1,w2,m,n-1,dp)});
     }
     int minDistance(string word1,string word2) {
         vector<vector<int>>dp(word1.length()+1,vector<int>(word2.length()+1,-1));
