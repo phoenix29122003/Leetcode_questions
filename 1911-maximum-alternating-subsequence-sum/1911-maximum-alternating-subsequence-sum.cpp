06691c685f4245df1bad760c98eaeef0e6de4b9f
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-05-28 16:25:07 +0530

    Time: 240 ms (43.47%), Space: 102.2 MB (49.68%) - LeetHub

---
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long dp[100001][2];
+    long long solve(vector<int>& nums,int idx,bool sign)
+    {
+        if(idx==nums.size()) return 0;
+        if(dp[idx][sign]!=-1) return dp[idx][sign];
+        long long curr=(sign?nums[idx]:-1*nums[idx]);
+        return dp[idx][sign]=max(curr+solve(nums,idx+1,!sign),solve(nums,idx+1,sign));
+    }
+    long long maxAlternatingSum(vector<int>& nums) {
+        memset(dp,-1,sizeof(dp));
+        return solve(nums,0,true);
+    }
+};
