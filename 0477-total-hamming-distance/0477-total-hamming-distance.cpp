39c938439c9d0babe56d7b88ddf99345ffbbe3fa
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-05-31 10:50:13 +0530

    Time: 57 ms (56.06%), Space: 19 MB (44.78%) - LeetHub

---
+++ 0477-total-hamming-distance/0477-total-hamming-distance.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int totalHammingDistance(vector<int>& nums) {
+        int ans=0;
+        for(int i=0;i<32;i++)
+        {
+            int one=0,zero=0;
+            for(auto it:nums)
+            {
+                if((it>>i)&1) one++;
+                else zero++;
+            }
+            ans+=one*zero;
+        }
+        return ans;
+    }
+};
