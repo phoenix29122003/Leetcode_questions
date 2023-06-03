8595a5726ae27eec610c8fe23a00e77e72ec87f6
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-06-03 18:29:30 +0530

    Time: 8 ms (55.36%), Space: 7.3 MB (6.82%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool solve(string s,string t)
+    {
+        map<char,char>mp;
+        for(int i=0;i<s.length();i++)
+        {
+            if(mp.find(s[i])!=mp.end())
+            {
+                if(mp[s[i]]!=t[i]) return false;
+            }
+            else mp[s[i]]=t[i];
+        }
+        return true;
+    }
+    bool isIsomorphic(string s, string t) {
+        return (solve(s,t) && solve(t,s));
+    }
+};
