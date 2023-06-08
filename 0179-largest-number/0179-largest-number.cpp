dcc1c1356f47d8255db2fff4b2bfda80b7090224
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-06-08 17:15:33 +0530

    Time: 9 ms (59.94%), Space: 11.4 MB (72.58%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    static bool compare(string a,string b){
-        return a+b > b+a;
+    static bool comp(string a,string b) 
+    {
+        return a+b>b+a;
     }
     string largestNumber(vector<int>& nums) {
-        vector<string>container;
-        for(int i:nums)  container.push_back(to_string(i));
-        sort(container.begin(),container.end(),compare);
-        string result;
-        for(int i=0;i<container.size();i++)  result+=container[i];
-        return result[0]=='0'? "0" : result;
+        vector<string>dum;
+        for(auto it:nums) dum.push_back(to_string(it));
+        sort(dum.begin(),dum.end(),comp);
+        string ans="";
+        for(auto it:dum) ans+=it;
+        if(ans[0]=='0') return "0";
+        else return ans;
     }
 };
