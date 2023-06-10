25f49fd0b6dd1c12d285b0dc294f77559dcdcc3e
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-06-10 06:33:14 +0530

    Time: 46 ms (8.20%), Space: 17.8 MB (48.10%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        int j=n-1;
-        vector<int>ans(n);
-        for(int i=1;i<n;i+=2) ans[i]=nums[j--];
-        for(int i=0;i<n;i+=2) ans[i]=nums[j--];
-        nums=ans;
+        priority_queue<int>pq(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();i+=2) nums[i]=pq.top(),pq.pop();
+        for(int i=0;i<nums.size();i+=2) nums[i]=pq.top(),pq.pop();
     }
 };
