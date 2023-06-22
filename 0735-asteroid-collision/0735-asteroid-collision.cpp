94873547d24091a98d410a72caece2997b9d14e7
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-06-22 19:23:58 +0530

    Time: 24 ms (11.22%), Space: 17.9 MB (26.11%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -6,17 +6,17 @@ public:
         for(int i=0;i<nums.size();i++)
         {
             if(nums[i]>0 || st.empty()) st.push(nums[i]);
-            else
+            else 
             {
-                while(!st.empty() && st.top()>0 && st.top()<abs(nums[i])) st.pop();
-                if(!st.empty() && st.top()==abs(nums[i])) st.pop();
+                while(st.size() && st.top()>0 && st.top()<abs(nums[i])) st.pop();
+                if(st.size() && st.top()>0 && st.top()==abs(nums[i])) st.pop();
                 else
                 {
                     if(st.empty() || st.top()<0) st.push(nums[i]);
                 }
             }
         }
-        while(!st.empty()) 
+        while(st.size())
         {
             ans.push_back(st.top());
             st.pop();
