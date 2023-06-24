abbdc59b4c2f6b2c06d16b33fe84228082004a1a
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-06-24 23:14:20 +0530

    Time: 3 ms (44.69%), Space: 8.2 MB (88.70%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -1,8 +1,9 @@
+
 class Solution {
 public:
     void solve(TreeNode *root,vector<int>&ans)
     {
-        if(!root) return;
+        if(root==NULL) return;
         solve(root->left,ans);
         ans.push_back(root->val);
         solve(root->right,ans);
