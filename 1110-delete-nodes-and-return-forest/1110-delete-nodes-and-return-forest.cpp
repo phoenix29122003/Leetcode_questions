cf7702e85a9eec88b69a82a5c80b2c9c0ca3ae98
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-06-27 16:55:49 +0530

    Time: 24 ms (72.99%), Space: 24.9 MB (98.10%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    vector<TreeNode*>ans;
-    set<int>dp;
-    void solve(TreeNode* &root)
+    void solve(TreeNode*&root,unordered_set<int>&set,vector<TreeNode*>&ans)
     {
-        if(root!=NULL)
+        if(root==NULL) return;
+        solve(root->left,set,ans);
+        solve(root->right,set,ans);
+        if(set.find(root->val)!=set.end())
         {
-            solve(root->left);
-            solve(root->right);
-            if(dp.find(root->val)!=dp.end())
-            {
-                if(root->left)  ans.push_back(root->left);
-                if(root->right) ans.push_back(root->right);
-                root=NULL;
-				delete root;
-            }
+            if(root->left) ans.push_back(root->left);
+            if(root->right) ans.push_back(root->right);
+            root=NULL;
+            delete root;
         }
     }
-    
     vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
-        for(int i=0;i<to_delete.size();i++) dp.insert(to_delete[i]);
-        solve(root);
+        unordered_set<int>set;
+        for(int it:to_delete) set.insert(it);
+        vector<TreeNode*>ans;
+        solve(root,set,ans);
         if(root) ans.push_back(root);
         return ans;
     }
