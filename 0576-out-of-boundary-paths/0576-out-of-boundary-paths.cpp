cca69f5a7d791b64d9ebeb0e8835c851fa1c0ef7
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-07-09 19:07:53 +0530

    Time: 10 ms (64.23%), Space: 6.5 MB (94.58%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int mod=pow(10,9)+7;
-    int solve(int m,int n,int N,int x,int y,vector<vector<vector<int>>>&dp){
+    int dp[51][51][51];
+    int solve(int m,int n,int N,int x,int y){
         if(x<0 || y<0 || x>=m || y>=n) return 1;
         if(N<=0) return 0;
         if(dp[N][x][y]!=-1) return dp[N][x][y]%mod;
         int ans=0;
-        ans=(ans+solve(m,n,N-1,x,y-1,dp))%mod;
-        ans=(ans+solve(m,n,N-1,x-1,y,dp))%mod;
-        ans=(ans+solve(m,n,N-1,x+1,y,dp))%mod;
-        ans=(ans+solve(m,n,N-1,x,y+1,dp))%mod;
+        ans=(ans+solve(m,n,N-1,x,y-1))%mod;
+        ans=(ans+solve(m,n,N-1,x-1,y))%mod;
+        ans=(ans+solve(m,n,N-1,x+1,y))%mod;
+        ans=(ans+solve(m,n,N-1,x,y+1))%mod;
         dp[N][x][y]=ans;
         return dp[N][x][y]%mod;
     }
     int findPaths(int m, int n, int N, int i, int j) {
-        vector<vector<vector<int>>>dp(N+1,vector<vector<int>>(m+1,vector<int>(n+1,-1)));
-        dp[N][m][n]=solve(m,n,N,i,j,dp);
+        memset(dp,-1,sizeof(dp));
+        dp[N][m][n]=solve(m,n,N,i,j);
         return dp[N][m][n];
     }
 };
