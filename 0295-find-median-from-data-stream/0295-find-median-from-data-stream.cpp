7cce417d35c7d6b954e2819612805065d2877c83
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-07-10 13:28:40 +0530

    Time: 405 ms (18.36%), Space: 116.9 MB (83.32%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -0,0 +1,21 @@
+class MedianFinder {
+public:
+    MedianFinder() {}
+    priority_queue<int>maxheap;
+    priority_queue<int,vector<int>,greater<int>>minheap;
+    void addNum(int num) {
+        maxheap.push(num);
+        minheap.push(maxheap.top());
+        maxheap.pop();
+        if(minheap.size()>maxheap.size())
+        {
+            maxheap.push(minheap.top());
+            minheap.pop();
+        }
+    }
+    
+    double findMedian() {
+        if(maxheap.size()>minheap.size()) return (double)maxheap.top();
+        else return (maxheap.top()+minheap.top())/2.0;
+    }
+};
