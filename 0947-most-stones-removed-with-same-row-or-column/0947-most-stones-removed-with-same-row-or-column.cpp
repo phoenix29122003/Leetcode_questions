593c179e74822c74bf20bd987f9b168ee0baa174
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-07-18 14:07:56 +0530

    Time: 316 ms (20.98%), Space: 14.1 MB (90.57%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -5,7 +5,7 @@ public:
         int result=0;
         for(int i=0;i<n;i++)
             if(!visited[i]&&(stones[i][0]==stones[index][0]||stones[i][1]==stones[index][1]))
-                result +=(dfs(stones,i,visited,n) + 1);
+                result+=(dfs(stones,i,visited,n)+1);
         return result;
     }
     int removeStones(vector<vector<int>>&stones) {
