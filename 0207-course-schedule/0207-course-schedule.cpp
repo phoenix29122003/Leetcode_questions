653410ace85d67ef28b2249fe9c2ed3d0c20ea00
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-07-22 15:10:04 +0530

    Time: 26 ms (49.99%), Space: 14.5 MB (20.01%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -3,12 +3,10 @@ public:
     bool iscycle(vector<vector<int>>&adj,vector<int>&vis,int id)
     {
         if(vis[id]==1) return true;
-        if(vis[id]==0)
-        {
+        if(vis[id]==0){
             vis[id]=1;
-            for(auto edge : adj[id]){
-                if(iscycle(adj,vis,edge))
-                    return true;
+            for(auto edge:adj[id]){
+                if(iscycle(adj,vis,edge)) return true;
             }
         }
         vis[id]=2;
@@ -16,11 +14,9 @@ public:
     }
     bool canFinish(int n, vector<vector<int>>& edge) {
         vector<vector<int>>adj(n);
-        for(auto it:edge)
-            adj[it[1]].push_back(it[0]);
+        for(auto it:edge) adj[it[1]].push_back(it[0]);
         vector<int>vis(n,0);
-        for(int i=0;i<n;i++)
-            if(iscycle(adj,vis,i)) return false;
+        for(int i=0;i<n;i++) if(iscycle(adj,vis,i)) return false;
         return true;
     }
 };
