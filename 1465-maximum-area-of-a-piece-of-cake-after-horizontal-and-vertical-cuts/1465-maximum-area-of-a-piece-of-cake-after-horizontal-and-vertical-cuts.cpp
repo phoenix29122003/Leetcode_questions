7f7563e2a2b747bc2385543a31a46bad776f6d5f
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-08-01 09:32:14 +0530

    Time: 57 ms (96.77%), Space: 32.2 MB (47.27%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     int maxArea(int a, int b, vector<int>& hc, vector<int>& vc) {
-        vector<int>h,v;
-        h.push_back(0),v.push_back(0);
-        for(int i=0;i<hc.size();i++) h.push_back(hc[i]);
-        for(int i=0;i<vc.size();i++) v.push_back(vc[i]);
-        h.push_back(a),v.push_back(b);
-        sort(h.begin(),h.end());
-        sort(v.begin(),v.end());
-        int vdiff=0,hdiff=0;
-        for(int i=1;i<h.size();i++) hdiff=max(hdiff,h[i]-h[i-1]);
-        for(int i=1;i<v.size();i++) vdiff=max(vdiff,v[i]-v[i-1]);
+        hc.insert(hc.begin(),0),hc.push_back(a);
+        vc.insert(vc.begin(),0),vc.push_back(b);
+        sort(hc.begin(),hc.end()),sort(vc.begin(),vc.end());
+        int hdiff=0,vdiff=0;
+        for(int i=1;i<hc.size();i++) hdiff=max(hdiff,hc[i]-hc[i-1]);
+        for(int i=1;i<vc.size();i++) vdiff=max(vdiff,vc[i]-vc[i-1]);
         return (1LL*hdiff*vdiff)%1000000007;
     }
 };
