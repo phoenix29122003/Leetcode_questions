240daa09428355a3a9e9b074a92eb263c487953e
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-08-01 19:31:35 +0530

    Time: 3 ms (39.29%), Space: 6 MB (57.70%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -2,12 +2,9 @@ class Solution {
 public:
     double myPow(double x, int n) {
         if(n==0) return 1;
-        else if(n<0)
-        {
-            n=abs(n);
-            x=1/x;
-        }
-        if(n%2) return x*myPow(x*x,n/2);
-        else return myPow(x*x,n/2);
+        if(n<0) x=1/x,n=abs(n);
+        if(n%2==0) return myPow(x*x,n/2);
+        else return x*myPow(x*x,n/2);
+        return 0;
     }
 };
