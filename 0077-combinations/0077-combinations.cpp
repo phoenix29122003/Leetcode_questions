dfe452320a602645cf5d600308f35e7ce0d45289
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-08-02 23:30:18 +0530

    Time: 288 ms (18.53%), Space: 149.7 MB (17.59%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    void solve(int idx,int n,int k,vector<int>&ds,vector<vector<int>>&ans)
-    {
-        if(ds.size()==k)
-        {
+    void solve(vector<int>nums,int idx,int k,vector<int>&ds,vector<vector<int>>&ans){
+        if(ds.size()==k) {
             ans.push_back(ds);
             return;
         }
-        for(int i=idx;i<=n;i++)
-        {
-            ds.push_back(i);
-            solve(i+1,n,k,ds,ans);
+        for(int i=idx;i<nums.size();i++){
+            ds.push_back(nums[i]);
+            solve(nums,i+1,k,ds,ans);
             ds.pop_back();
         }
     }
     vector<vector<int>> combine(int n, int k) {
-        vector<vector<int>>ans;
+        vector<int>nums;
+        for(int i=1;i<=n;i++) nums.push_back(i);
         vector<int>ds;
-        solve(1,n,k,ds,ans);
+        vector<vector<int>>ans;
+        solve(nums,0,k,ds,ans);
         return ans;
     }
 };
