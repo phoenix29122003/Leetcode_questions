d54c37c6870c96b42ac7716fe15e6ef8373b6231
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-08-03 16:21:03 +0530

    Time: 248 ms (32.49%), Space: 124.6 MB (23.40%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    bool isuniq(string str)
-    {
-        set<char>s;
-        for(int i=0;i<str.length();i++)
-        {
-            if(s.find(str[i])!=s.end()) return false;
-            else s.insert(str[i]);
-        }
-        return true;
+    bool isunique(string s){
+        set<char>set;
+        for(char ch:s) set.insert(ch);
+        return s.length()==set.size();
     }
-    void solve(vector<string>&arr,int idx,string ds,int &ans)
-    {
-        if(!isuniq(ds)) return;
+    void solve(vector<string>arr,int idx,string ds,int &ans){
+        if(!isunique(ds)) return;
         if(ans<ds.length()) ans=ds.length();
-        for(int i=idx;i<arr.size();i++)
-        {
+        for(int i=idx;i<arr.size();i++){
             solve(arr,i+1,ds+arr[i],ans);
         }
     }
     int maxLength(vector<string>& arr) {
         int ans=0;
-        string ds="";
-        solve(arr,0,ds,ans);
+        solve(arr,0,"",ans);
         return ans;
     }
 };
