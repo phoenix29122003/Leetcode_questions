df8e7f7e12df45680b386c88b4819aafff9d6058
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-08-03 19:19:02 +0530

    Time: 4 ms (93.24%), Space: 14.1 MB (36.50%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
-    void solve(vector<vector<int>>& image,int sr,int sc,int col,int val,int n,int m)
-    {
-        if(sr<0 || sc<0 || sr>=n || sc>=m || image[sr][sc]==col || image[sr][sc]!=val) return;
-            image[sr][sc]=col;
-            solve(image,sr+1,sc,col,val,n,m);
-            solve(image,sr-1,sc,col,val,n,m);
-            solve(image,sr,sc+1,col,val,n,m);
-            solve(image,sr,sc-1,col,val,n,m);
+    void solve(vector<vector<int>>& grid,int i,int j,int color1,int m,int n,int color2){
+        if(i<0 || j<0 || i>=m || j>=n || grid[i][j]!=color1 || grid[i][j]==color2) return;
+        grid[i][j]=color2;
+        solve(grid,i-1,j,color1,m,n,color2);
+        solve(grid,i+1,j,color1,m,n,color2);
+        solve(grid,i,j-1,color1,m,n,color2);
+        solve(grid,i,j+1,color1,m,n,color2);
     }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) 
-    {
-        int n=image.size();
-        int m=image[0].size();
-        int val=image[sr][sc];
-        solve(image,sr,sc,color,val,n,m);
-        return image;
+    vector<vector<int>> floodFill(vector<vector<int>>& grid, int sr, int sc, int color) {
+        solve(grid,sr,sc,grid[sr][sc],grid.size(),grid[0].size(),color);
+        return grid;
     }
 };
