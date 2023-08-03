08e2b8d4cb78ae2f54bcd8ec9a14ae8fc75f28e6
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-08-03 15:01:03 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (18.80%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     vector<string>mp={"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"},ans;
     void solve(string digits,int idx,string ds){
-        if(idx==digits.size()){
+        if(ds.length()==digits.length()){
             ans.push_back(ds);
             return;
         }
-        for(char &ch:mp[digits[idx]-'2']){
+        for(char ch:mp[digits[idx]-'2']){
             ds.push_back(ch);
             solve(digits,idx+1,ds);
             ds.pop_back();
@@ -14,7 +14,8 @@ public:
     }
     vector<string> letterCombinations(string digits) {
         if(digits=="") return {};
-        solve(digits,0,"");
+        string ds="";
+        solve(digits,0,ds);
         return ans;
     }
 };
