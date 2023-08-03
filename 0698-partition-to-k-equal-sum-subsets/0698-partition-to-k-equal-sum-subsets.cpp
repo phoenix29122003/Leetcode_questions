1e68bbcc3c1787f33e0bafae8cced61208ec12db
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-08-03 15:40:42 +0530

    Time: 678 ms (44.04%), Space: 9.5 MB (44.75%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
-bool solve(vector<int>& nums,int idx,int sum,int curr,vector<bool>&vis,int k)
+    bool solve(vector<int>& nums,int idx,int sum,int curr,vector<bool>&vis,int k)
     {
         if(k==1) return true;
         if(idx>=nums.size()) return false;
         if(curr==sum) return solve(nums,0,sum,0,vis,k-1);
-        for(int i=idx;i<nums.size();i++)
-        {
-            if(!vis[i] && curr+nums[i]<=sum)
-            {
+        for(int i=idx;i<nums.size();i++){
+            if(!vis[i] && curr+nums[i]<=sum){
                 vis[i]=true;
                 if(solve(nums,i+1,sum,curr+nums[i],vis,k)) return true;
                 vis[i]=false;
