5788ea39356f3d24e96dc9353bb71345faf1c2f5
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-08-03 23:23:17 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (71.89%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
-    int solve(int n,vector<int>&dp)
-    {
-        if(n<=2) return dp[n]=n;
-        if(dp[n]!=0) return dp[n];
-        else return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+    int dp[46];
+    int solve(int n){
+        if(n==0) return 1;
+        if(n<0) return 0;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=solve(n-1)+solve(n-2);
     }
     int climbStairs(int n) {
-        vector<int>dp(46,0);
-        return solve(n,dp);
+        memset(dp,-1,sizeof(dp));
+        return solve(n);
     }
 };
