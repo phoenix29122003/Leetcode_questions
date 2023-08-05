37c8371a14b1a889fc44f5e62879a6056337df6a
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-08-05 13:38:16 +0530

    Time: 13 ms (70.31%), Space: 12.6 MB (70.63%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int dp[20001];
-    int solve(vector<int>nums,int idx)
-    {
+    int solve(vector<int>&nums,int idx){
         if(idx>=nums.size()) return 0;
         if(dp[idx]!=-1) return dp[idx];
         int curr=nums[idx];
-        int sum=nums[idx];
-        int i=idx+1;
-        while(i<nums.size() && nums[i]==curr) sum+=nums[i++];
+        int i=idx,sum=0;
+        while(i<nums.size() && nums[i]==curr) i++,sum+=curr;
         int j=i;
         while(j<nums.size() && nums[j]==curr+1) j++;
-        return dp[idx]=max(sum+solve(nums,j),solve(nums,i));
+        int nottake=solve(nums,i);
+        int take=sum+solve(nums,j);
+        return dp[idx]=max(take,nottake);
     }
     int deleteAndEarn(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
         sort(nums.begin(),nums.end());
+        memset(dp,-1,sizeof(dp));
         return solve(nums,0);
     }
 };
