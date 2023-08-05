693a15da4e86fb3cc7f65db9b6ea0f23ca28162a
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-08-05 10:07:37 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (21.18%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -7,8 +7,7 @@ public:
         else if(dp[idx]!=-1) return dp[idx];
         int ans=0;
         if(s[idx]>='1' && s[idx]<='9') ans+=solve(s,idx+1,n);
-        if(s[idx]=='1' && (s[idx+1]>='0' && s[idx+1]<='9')) ans+=solve(s,idx+2,n);
-        if(s[idx]=='2' && (s[idx+1]>='0' && s[idx+1]<='6')) ans+=solve(s,idx+2,n);
+        if((s[idx]=='1' && (s[idx+1]>='0' && s[idx+1]<='9'))||(s[idx]=='2' && (s[idx+1]>='0' && s[idx+1]<='6'))) ans+=solve(s,idx+2,n);
         return dp[idx]=ans;
     }
     int numDecodings(string s) {
