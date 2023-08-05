41e1d20f7dffad445f0b070abfdba225c30dd773
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-08-05 10:06:37 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (15.78%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    int solve(string s,int idx,vector<int>&dp)
-    {
-        if(idx>=s.length()) return 1;
-        if(dp[idx]!=0) return dp[idx];
-        if(idx<s.length() && s[idx]=='0') return 0;
+    int dp[101];
+    int solve(string s,int idx,int n){
+        if(idx>n) return 0;
+        else if(idx==n) return 1;
+        else if(dp[idx]!=-1) return dp[idx];
         int ans=0;
-        if(s[idx]>='1' && s[idx]<='9') ans+=solve(s,idx+1,dp);
-        if(idx+1<s.length())
-        {
-            if((s[idx]=='1' && (s[idx+1]>='0' && s[idx+1]<='9') )||(s[idx]=='2' && (s[idx+1]>=0 && s[idx+1]<='6'))) ans+=solve(s,idx+2,dp);
-        }
+        if(s[idx]>='1' && s[idx]<='9') ans+=solve(s,idx+1,n);
+        if(s[idx]=='1' && (s[idx+1]>='0' && s[idx+1]<='9')) ans+=solve(s,idx+2,n);
+        if(s[idx]=='2' && (s[idx+1]>='0' && s[idx+1]<='6')) ans+=solve(s,idx+2,n);
         return dp[idx]=ans;
     }
     int numDecodings(string s) {
-        vector<int>dp(101,0);
-        return solve(s,0,dp);
+        memset(dp,-1,sizeof(dp));
+        return solve(s,0,s.length());
     }
 };
