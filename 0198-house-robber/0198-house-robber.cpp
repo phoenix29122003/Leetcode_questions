e93889110371254f610cad78e7ed015299ef9b64
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-08-05 19:38:12 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (73.75%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int solve(vector<int>&nums,int idx,vector<int>&dp)
-    {
-        if(idx>=nums.size()) return 0;
+    int dp[101];
+    int solve(vector<int>&nums,int idx,int n){
+        if(idx>=n) return 0;
         if(dp[idx]!=-1) return dp[idx];
-        int take=nums[idx]+solve(nums,idx+2,dp);
-        int nottake=0+solve(nums,idx+1,dp);
+        int take=nums[idx]+solve(nums,idx+2,n);
+        int nottake=solve(nums,idx+1,n);
         return dp[idx]=max(take,nottake);
     }
     int rob(vector<int>& nums) {
-        vector<int>dp(nums.size()+1,-1);
-        return solve(nums,0,dp);
+        memset(dp,-1,sizeof(dp));
+        return solve(nums,0,nums.size());
     }
 };
