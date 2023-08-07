252780104b37b1c3cb9be701c5ae90da24f04445
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-08-07 22:48:53 +0530

    Time: 42 ms (86.74%), Space: 11 MB (71.49%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string &text1, string &text2,int m,int n,int memo[][1001]) 
+    int dp[1001][1001];
+    int lcs(string &text1, string &text2,int m,int n) 
     {
         if(m==0 || n==0) return 0;
-        if(memo[m][n]!=-1) return memo[m][n];
-        if(text1[m-1]==text2[n-1]) return memo[m][n]= 1+longestCommonSubsequence(text1,text2,m-1,n-1,memo);
-        else
-        {
-            return memo[m][n]= max(longestCommonSubsequence(text1,text2,m-1,n,memo),longestCommonSubsequence(text1,text2,m,n-1,memo));
-        }
+        if(dp[m][n]!=-1) return dp[m][n];
+        if(text1[m-1]==text2[n-1]) return dp[m][n]=1+lcs(text1,text2,m-1,n-1);
+        else return dp[m][n]= max(lcs(text1,text2,m-1,n),lcs(text1,text2,m,n-1));
     }
     int minInsertions(string s) {
         string t=s;
         reverse(t.begin(),t.end());
         int m=s.size(),n=t.size();
-        int memo[1001][1001];
-        for(int i=0;i<1001;i++){
-            for(int j=0;j<1001;j++){
-                memo[i][j]=-1;
-            }
-        }
-        return s.length()-longestCommonSubsequence(s,t,m,n,memo);
+        memset(dp,-1,sizeof(dp));
+        return s.length()-lcs(s,t,m,n);
     }
 };
