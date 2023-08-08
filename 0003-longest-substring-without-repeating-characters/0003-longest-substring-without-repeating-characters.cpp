f2d45a3562978baf8de6799624abc489565296f2
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-08-08 13:08:50 +0530

    Time: 26 ms (46.27%), Space: 10.8 MB (23.84%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,20 +4,15 @@ public:
         unordered_set<char>set;
         int ans=0,i=0,j=0,n=s.length();
         while(j<n){
-            if(set.find(s[j])==set.end()){
-                set.insert(s[j]);
-                ans=max(ans,j-i+1);
-                j++;
-            }
-            else {
+            if(set.find(s[j])!=set.end()){
                 while(set.find(s[j])!=set.end()){
                     set.erase(s[i]);
                     i++;
                 }
-                set.insert(s[j]);
-                ans=max(ans,j-i+1);
-                j++;
             }
+            set.insert(s[j]);
+            ans=max(ans,j-i+1);
+            j++;
         }
         return ans;
     }
