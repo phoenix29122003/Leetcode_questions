c93333ca396dd2f011977bc05daa0f8918e85663
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-08-09 22:04:35 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (5.26%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int cnt=0,ans=0;
+        for(auto it:s){
+            if(it=='R') cnt++;
+            else cnt--;
+            if(cnt==0) ans++;
+        }
+        return ans;
+    }
+};
