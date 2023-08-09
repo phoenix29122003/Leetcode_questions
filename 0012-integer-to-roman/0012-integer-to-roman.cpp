e67786d2b77daf6dce0dea47b7e0b3abb9c94db4
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-08-09 13:34:30 +0530

    Time: 8 ms (63.41%), Space: 6 MB (50.73%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -5,6 +5,6 @@ public:
         string tens[]={"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
         string hrns[]={"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
         string ths[]={"","M","MM","MMM"};
-        return ths[num/1000]+hrns[(num%1000)/100]+tens[(num%100)/10]+ones[num%10];
+        return ths[((num)/1000)%10]+hrns[((num)/100)%10]+tens[((num)/10)%10]+ones[(num/1)%10];
     }
 };
