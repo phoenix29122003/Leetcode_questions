7121741259edfca2f976615c330db5757cfd77e9
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-08-13 21:41:14 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (38.32%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    unordered_map<long long,int>mp;
+    long long solve(int n){
+        if(n==1) return 0;
+        if(mp.find(n)!=mp.end()) return mp[n];
+        if(n%2==0) return mp[n]=1+solve(n/2);
+        else return mp[n]=2+min(solve((n)/2),solve(n/2+1));
+    }
+    int integerReplacement(int n) {
+        return solve(n);
+    }
+};
