986b454642d9d67124d508d06da6ede5802b35b9
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-08-18 15:12:31 +0530

    Time: 4 ms (54.90%), Space: 7.5 MB (42.20%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -3,7 +3,7 @@ public:
     int dp[21][21];
     int solve(vector<int>&nums,int i,int j){
         int n=nums.size();
-        if((i>j)||(i>=n)||(j<0)) return 0;
+        if(i>j) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
         int op1=nums[i]+min(solve(nums,i+2,j),solve(nums,i+1,j-1));
         int op2=nums[j]+min(solve(nums,i,j-2),solve(nums,i+1,j-1));
