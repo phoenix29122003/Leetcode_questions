5d84e0639da5b9fbef0452023bf4323e7170bf51
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-08-19 16:37:32 +0530

    Time: 4 ms (57.57%), Space: 13.4 MB (61.18%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
-    int solve(TreeNode *root,int &sum,bool chk)
-    {
-        if(!root) return 0;
-        solve(root->left,sum,true);
-        solve(root->right,sum,false);
-        if(root->left==NULL && root->right==NULL && chk==true) sum+=root->val;
-        return sum;
+    void solve(TreeNode *root,int left,int &ans){
+        if(root==NULL) return;
+        if(root->left==NULL && root->right==NULL && left) ans+=root->val;
+        solve(root->left,1,ans);
+        solve(root->right,0,ans);
     }
     int sumOfLeftLeaves(TreeNode* root) {
-        bool chk=false;
-        int sum=0;
-        solve(root,sum,chk);
-        return sum;
+        int ans=0;
+        solve(root,0,ans);
+        return ans;
     }
 };
