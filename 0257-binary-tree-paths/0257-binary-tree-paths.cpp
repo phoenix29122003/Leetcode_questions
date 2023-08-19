d067c222865a18b73980c227df044128d7072664
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-08-19 14:47:53 +0530

    Time: 0 ms (100.00%), Space: 13.7 MB (33.39%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    void solve(TreeNode *root,string ds,vector<string>&ans)
-    {
+    void solve(TreeNode *root,string ds,vector<string>&ans){
         if(root==NULL) return;
-        if(root->left||root->right) ds+=to_string(root->val)+"->";
-        else 
-        {
+        else if(root->left==NULL && root->right==NULL){
             ds+=to_string(root->val);
             ans.push_back(ds);
+            return;
         }
+        ds+=to_string(root->val)+"->";
         solve(root->left,ds,ans);
         solve(root->right,ds,ans);
     }
     vector<string> binaryTreePaths(TreeNode* root) {
         vector<string>ans;
-        string ds="";
-        solve(root,ds,ans);
+        solve(root,"",ans);
         return ans;
     }
 };
