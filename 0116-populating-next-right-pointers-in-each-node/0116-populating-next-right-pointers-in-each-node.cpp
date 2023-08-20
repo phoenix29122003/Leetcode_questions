9806e5ca339a1a49b038ef36cd668b30e1cbb8f7
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-08-21 01:12:11 +0530

    Time: 11 ms (97.45%), Space: 17.2 MB (17.20%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -9,10 +9,7 @@ public:
             for(int i=0;i<n;i++){
                 auto curr=q.front();
                 q.pop();
-                if(n-i>=2){
-                    auto next=q.front();
-                    curr->next=next;
-                }
+                if(i!=n-1) curr->next=q.front();
                 if(curr->left) q.push(curr->left);
                 if(curr->right) q.push(curr->right);
             }
