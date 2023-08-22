f4578eeb4817f9254abe7a4b065f222798d9b004
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-08-22 19:12:29 +0530

    Time: 181 ms (57.88%), Space: 112.5 MB (43.78%) - LeetHub

--- 1673-find-the-most-competitive-subsequence/1673-find-the-most-competitive-subsequence.cpp
+++ 1673-find-the-most-competitive-subsequence/1673-find-the-most-competitive-subsequence.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     vector<int> mostCompetitive(vector<int>& nums, int k) {
+        int n=nums.size();
         vector<int>ans;
         stack<int>st;
-        for(int i=0;i<nums.size();i++)
-        {
-            while(st.size() && st.top()>nums[i] && nums.size()-i-1>=k-st.size()) st.pop();
-            if(st.size()<k) st.push(nums[i]);
+        for(int i=0;i<nums.size();i++){
+            if(st.empty() || st.top()<=nums[i]) st.push(nums[i]);
+            else{
+                while(st.size() && st.size()+(n-i)>k && st.top()>nums[i]) st.pop();
+                st.push(nums[i]);
+            }
+            if(st.size()>k) st.pop();
         }
-        while(st.size())
-        {
+        while(st.size()){
             ans.push_back(st.top());
             st.pop();
         }
