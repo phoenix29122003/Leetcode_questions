113df564b0972fb11ac6aadd881c64825422c812
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-09-07 10:31:58 +0530

    Time: 47 ms (51.93%), Space: 7.4 MB (98.17%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
-        int n=points.size();
-        if(n<=2) return n;
-        int ans=0;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i+1;j<n;j++)
-            {
-                int x1=points[i][0];
-                int y1=points[i][1];
-                int x2=points[j][0];
-                int y2=points[j][1];
-                int total=2;
-                for(int k=0;k!=i && k!=j && k<n;k++)
-                {
-                    int x=points[k][0];
-                    int y=points[k][1];
-                    if((x2-x1)*(y1-y)==(x1-x)*(y2-y1)) total++;
+        int n=points.size(),ans=0;
+        if(n==1) return 1;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int cnt=2,slope;
+                int x1=points[i][0],x2=points[j][0],y1=points[i][1],y2=points[j][1];
+                for(int k=0;k!=i &&k!=j&&k<n;k++){
+                    int x=points[k][0],y=points[k][1];
+                    if((y2-y1)*(x1-x)==(x2-x1)*(y1-y)) cnt++;
                 }
-                ans=max(ans,total);
+                ans=max(ans,cnt);
             }
         }
         return ans;
