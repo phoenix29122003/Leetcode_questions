9b5758451a326f6402f6de90a48b930536c70d1d
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-09-08 17:50:22 +0530

    Time: 52 ms (54.59%), Space: 21.8 MB (52.76%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.cpp
+++ 0934-shortest-bridge/0934-shortest-bridge.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     queue<pair<int,int>>q;   
-    void dfs(vector<vector<int>>& grid,int i,int j) 
-    {   
+    void dfs(vector<vector<int>>& grid,int i,int j) {   
         if(i<0 or i>=grid.size() or j<0 or j>=grid[0].size() or grid[i][j]==2 or grid[i][j]==0) return; 
         grid[i][j]=2; 
         q.push({i,j});
@@ -11,25 +10,20 @@ public:
         dfs(grid,i,j+1);
         dfs(grid,i,j-1);
     }
-    int bfs(vector<vector<int>>& grid)
-    {   
+    int bfs(vector<vector<int>>& grid){   
         int d=0; 
         int mindist=INT_MAX;
         vector<vector<int>>dir={{1,0},{0,1},{-1,0},{0,-1}};
-        while(!q.empty())
-        {
+        while(!q.empty()){
             int n=q.size();
-            while(n--)
-            {
+            while(n--){
                 auto a=q.front();
                 q.pop();
-                for(int h=0;h<4;h++) 
-                {
+                for(int h=0;h<4;h++) {
                     int x=dir[h][0]+a.first;
                     int y=dir[h][1]+a.second;
                     if(x>=0 and x<grid.size() and y>=0 and y<grid[0].size() and grid[x][y]==1) mindist=min(mindist,d);
-                    else if(x>=0 and x<grid.size() and y>=0 and y<grid[0].size() and grid[x][y]==0) 
-                    { 
+                    else if(x>=0 and x<grid.size() and y>=0 and y<grid[0].size() and grid[x][y]==0) { 
                         q.push({x, y});
                         grid[x][y]=2;
                     }
