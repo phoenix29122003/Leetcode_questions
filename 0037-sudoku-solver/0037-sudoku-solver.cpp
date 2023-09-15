294690ca6857c19b9535b52d80b55e974a2edbca
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-09-15 21:19:57 +0530

    Time: 26 ms (53.79%), Space: 6.7 MB (9.95%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
-    bool issafe(vector<vector<char>>&board,int row,int col,char c)
-    {
-        for(int i=0;i<9;i++)
-        {
-            if(board[i][col]==c) return false;
-            if(board[row][i]==c) return false;
-            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c) return false;
-        }
+    bool issafe(vector<vector<char>>& board,int r,int c,char ch){
+        for(int i=0;i<9;i++){
+            if(board[i][c]==ch) return false;
+            if(board[r][i]==ch) return false;
+            if(board[3*(r/3)+i/3][3*(c/3)+i%3]==ch) return false;
+        } 
         return true;
     }
-    bool solve(vector<vector<char>>&board,int m,int n)
-    {
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(board[i][j]=='.')
-                {
-                    for(int c='1';c<='9';c++)
-                    {
-                        if(issafe(board,i,j,c))
-                        {
-                            board[i][j]=c;
-                            if(solve(board,m,n)) return true;
-                            else board[i][j]='.';
+    bool solve(vector<vector<char>>& board){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.'){
+                    for(char ch='1';ch<='9';ch++){
+                        if(issafe(board,i,j,ch)){
+                            board[i][j]=ch;
+                            if(solve(board)) return true;
+                            board[i][j]='.';
                         }
                     }
                     return false;
@@ -34,7 +26,6 @@ public:
         return true;
     }
     void solveSudoku(vector<vector<char>>& board) {
-        int m=board.size(),n=board[0].size();
-        solve(board,m,n);
+        solve(board);
     }
 };
