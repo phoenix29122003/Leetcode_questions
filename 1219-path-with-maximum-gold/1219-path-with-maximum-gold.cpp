6c2a53a9f7e08f029e7a8cd273bc81261f24e1b1
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-09-15 23:09:23 +0530

    Time: 247 ms (52.31%), Space: 7.6 MB (24.03%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -4,9 +4,9 @@ public:
         if(i<0 || j<0 || i>=m || j>=n || grid[i][j]==0) return 0;
         int temp=grid[i][j];
         grid[i][j]=0;
-        int ans=temp+max({dfs(grid,i-1,j,m,n),dfs(grid,i+1,j,m,n),dfs(grid,i,j-1,m,n),dfs(grid,i,j+1,m,n)});
+        int maxi=max({dfs(grid,i-1,j,m,n),dfs(grid,i+1,j,m,n),dfs(grid,i,j-1,m,n),dfs(grid,i,j+1,m,n)});
         grid[i][j]=temp;
-        return ans;
+        return maxi+temp;
     }
     int getMaximumGold(vector<vector<int>>& grid) {
         int ans=0,m=grid.size(),n=grid[0].size();
