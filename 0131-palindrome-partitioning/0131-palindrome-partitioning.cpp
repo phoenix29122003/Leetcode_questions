f6b47bd46b81a267114e32904393c719c01fc4c1
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-09-16 11:43:53 +0530

    Time: 105 ms (58.02%), Space: 54 MB (69.02%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    bool ispal(string s)
-    {
+    bool ispal(string s){
         string t=s;
         reverse(t.begin(),t.end());
         return t==s;
     }
-    void solve(string s,int idx,vector<string>&ds,vector<vector<string>>&ans)
-    {
-        if(idx==s.length())
-        {
+    void solve(string s,int idx,vector<string>&ds,vector<vector<string>>&ans){
+        if(idx==s.length()){
             ans.push_back(ds);
             return;
         }
-        for(int i=idx;i<s.length();i++)
-        {
-            if(ispal(s.substr(idx,i-idx+1)))
-            {
+        for(int i=idx;i<s.length();i++){
+            if(ispal(s.substr(idx,i-idx+1))){
                 ds.push_back(s.substr(idx,i-idx+1));
                 solve(s,i+1,ds,ans);
                 ds.pop_back();
