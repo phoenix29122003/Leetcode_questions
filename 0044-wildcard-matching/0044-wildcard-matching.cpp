29c9153c9e40044c7c261be17822312ef9bd8760
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-09-16 14:16:50 +0530

    Time: 2196 ms (5.00%), Space: 775.7 MB (5.43%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    bool solve(string s,string p,int i,int j,int m,int n,vector<vector<int>>&dp)
-    {
+    int dp[2001][2001];
+    int solve(string s,string p,int i,int j,int m,int n){
         if(j==n) return i==m;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(p[j]=='*') return dp[i][j]=solve(s,p,i,j+1,m,n,dp)||(i<m && solve(s,p,i+1,j,m,n,dp));
-        if(i<m && (p[j]=='?' || s[i]==p[j])) return dp[i][j]=solve(s,p,i+1,j+1,m,n,dp);
+        if(s[i]!=p[j] && p[j]=='*') return dp[i][j]=solve(s,p,i,j+1,m,n)||(i<m && solve(s,p,i+1,j,m,n));
+        if(i<m && (s[i]==p[j] || p[j]=='?')) return dp[i][j]=solve(s,p,i+1,j+1,m,n);
         return dp[i][j]=0;
     }
     bool isMatch(string s, string p) {
-        vector<vector<int>>dp(s.length()+1,vector<int>(p.length()+1,-1));
-        return solve(s,p,0,0,s.length(),p.length(),dp);
+        memset(dp,-1,sizeof(dp));
+        return solve(s,p,0,0,s.length(),p.length());
     }
 };
