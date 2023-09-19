e13222fd902940e12ac385c3c6d73df455ddb74b
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-09-19 15:24:07 +0530

    Time: 359 ms (31.27%), Space: 172.5 MB (5.10%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int dp[21][4000];
-    int solve(vector<int>nums,int idx,int n,int target){
-        if(idx==n && target==0) return 1;
-        if(idx==n && target!=0) return 0;
+    int solve(vector<int>nums,int idx,int target){
+        if(idx==nums.size()) return target==0;
         if(dp[idx][target+1000]!=-1) return dp[idx][target+1000];
-        return dp[idx][target+1000]=solve(nums,idx+1,n,target-nums[idx])+solve(nums,idx+1,n,target+nums[idx]);
+        return dp[idx][target+1000]=solve(nums,idx+1,target-nums[idx])+solve(nums,idx+1,target+nums[idx]);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         memset(dp,-1,sizeof(dp));
-        return solve(nums,0,nums.size(),target);
+        return solve(nums,0,target);
     }
 };
