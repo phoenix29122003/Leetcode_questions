0e4019533ed376cc05a1cd616da97a7475d845d7
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-09-20 18:54:57 +0530

    Time: 174 ms (32.31%), Space: 58.1 MB (13.25%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
-    bool ispossible(vector<int>& arr,int mid,int m)
-    {
-        int cnt=1;
-        int curr=arr[0];
-        for(int i=1;i<arr.size();i++)
-        {
-            if(arr[i]-curr>=mid) 
-            {
+    bool ispossible(vector<int>&nums,int n,int diff){
+        int cnt=1,i=0,j=1;
+        while(j<nums.size()){
+            if(nums[j]-nums[i]>=diff){
                 cnt++;
-                curr=arr[i];
-                if(cnt==m) return true;
+                i=j;
             }
+            j++;
         }
-        return cnt>=m?true:false;
+        return cnt>=n;
     }
-    int maxDistance(vector<int>& arr, int m) {
-        sort(arr.begin(),arr.end());
-        int n=arr.size(),s=0,e=arr[n-1],ans;
-        while(s<=e)
-        {
-            int mid=(s+e)/2;
-            if(ispossible(arr,mid,m))
-            {
+    int maxDistance(vector<int>& nums, int m) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size(),ans;
+        int l=0,h=nums[n-1]-nums[0];
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(ispossible(nums,m,mid)){
                 ans=mid;
-                s=mid+1;
+                l=mid+1;
             }
-            else e=mid-1;
+            else h=mid-1;
         }
         return ans;
     }
