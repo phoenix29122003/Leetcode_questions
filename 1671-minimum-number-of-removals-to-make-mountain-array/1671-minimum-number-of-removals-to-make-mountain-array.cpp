c26c31aacd191c86b2659386125be863405754e9
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-09-23 16:28:06 +0530

    Time: 245 ms (43.92%), Space: 12.5 MB (23.17%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int minimumMountainRemovals(vector<int>& nums) {
-        vector<int>ldp(nums.size(),1);
-        vector<int>rdp(nums.size(),1);
-        for(int i=1;i<nums.size();i++)
+        int n=nums.size();
+        vector<int>ldp(n,1),rdp(n,1);
+        
+        for(int i=1;i<n;i++)
             for(int j=0;j<i;j++)
                 if(nums[i]>nums[j]) ldp[i]=max(ldp[i],ldp[j]+1);
-        for(int i=nums.size()-2;i>=0;i--)
-            for(int j=nums.size()-1;j>i;j--)
+        
+        for(int i=n-2;i>=0;i--)
+            for(int j=n-1;j>i;j--)
                 if(nums[i]>nums[j]) rdp[i]=max(rdp[i],rdp[j]+1);
+        
         int maxi=0;
-        for(int i=1;i<nums.size()-1;i++)
-        {
-            if(ldp[i]>1 && rdp[i]>1)
-            {
+        for(int i=1;i<n-1;i++){
+            if(ldp[i]>1 && rdp[i]>1){
                 int ans=ldp[i]+rdp[i]-1;
                 maxi=max(maxi,ans);
             }
         }
-        return nums.size()-maxi;
+        return n-maxi;
     }
 };
