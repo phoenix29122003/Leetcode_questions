7ac7865842f105971dbd9476a128529127c1d691
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-09-27 16:52:59 +0530

    Time: 11 ms (50.30%), Space: 8.9 MB (46.89%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
+    int dp[501][501];
+    int solve(vector<int>& nums,int l,int r){
+        if(r<l) return 0;
+        if(dp[l][r]!=-1) return dp[l][r];
+        int op1=nums[l]+min(solve(nums,l+2,r),solve(nums,l+1,r-1));
+        int op2=nums[r]+min(solve(nums,l+1,r-1),solve(nums,l,r-2));
+        return dp[l][r]=max(op1,op2);
+    }
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int sum=0;
+        memset(dp,-1,sizeof(dp));
+        for(auto it:piles) sum+=it;
+        int ans=solve(piles,0,piles.size()-1);
+        return ans>(sum-ans);
     }
 };
