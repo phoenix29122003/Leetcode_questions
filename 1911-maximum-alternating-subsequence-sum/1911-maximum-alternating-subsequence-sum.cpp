e72b44d6bdc404c0cad742dcce7d223a4c60bca0
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-09-27 16:27:32 +0530

    Time: 136 ms (82.14%), Space: 100.9 MB (72.35%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     long long dp[100001][2];
-    long long solve(vector<int>&nums,int idx,bool sign)
-    {
-        if(idx==nums.size()) return 0;
-        if(dp[idx][sign]!=-1) return dp[idx][sign];
-        long long curr=sign?nums[idx]:-1*nums[idx];
-        return dp[idx][sign]=max(curr+solve(nums,idx+1,!sign),solve(nums,idx+1,sign));
+    long long solve(vector<int>&nums,int idx,int n,int pos){
+        if(idx>=n) return 0;
+        if(dp[idx][pos]!=-1) return dp[idx][pos];
+        long long pick,notpick;
+        if(pos) pick=nums[idx]+solve(nums,idx+1,n,0);
+        else pick=-nums[idx]+solve(nums,idx+1,n,1);
+        notpick=solve(nums,idx+1,n,pos);
+        return dp[idx][pos]=max(pick,notpick);
     }
     long long maxAlternatingSum(vector<int>& nums) {
         memset(dp,-1,sizeof(dp));
-        return solve(nums,0,true);
+        return solve(nums,0,nums.size(),1);
     }
 };
