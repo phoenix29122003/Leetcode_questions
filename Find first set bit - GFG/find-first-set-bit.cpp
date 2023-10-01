90cae2ac75d2135979436a2af05ff16ffeea2fba
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-10-01 23:49:54 +0530

    Added solution - LeetHub

--- Find first set bit - GFG/find-first-set-bit.cpp
+++ Find first set bit - GFG/find-first-set-bit.cpp
@@ -15,15 +15,7 @@ class Solution
     //Function to find position of first set bit in the given number.
     unsigned int getFirstSetBit(int n)
     {
-        // Your code here
-        if(n==0) return 0;
-        int m=1,ans=1;
-        while(!(m&n))
-        {
-            m=m<<1;
-            ans++;
-        }
-        return ans;
+        return log2(n&-n)+1;
     }
 };
 
