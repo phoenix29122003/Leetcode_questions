8dde80ab96aa0948384317b917bc6b444fcc404b
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-10-04 13:02:38 +0530

    Time: 7 ms (25.08%), Space: 8.9 MB (5.94%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         vector<int>ans;
-        for(int i=0;i<=n;i++) ans.push_back(__builtin_popcount(i));
+        for(int i=0;i<=n;i++){
+            ans.push_back(__builtin_popcount(i));
+        }
         return ans;
     }
 };
