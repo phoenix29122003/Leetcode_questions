4bf46e03f1c7f978521f71fde76ed274b438c889
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-10-17 15:44:59 +0530

    Time: 63 ms (93.53%), Space: 58.5 MB (32.13%) - LeetHub

--- 0954-array-of-doubled-pairs/0954-array-of-doubled-pairs.cpp
+++ 0954-array-of-doubled-pairs/0954-array-of-doubled-pairs.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
-    bool canReorderDoubled(vector<int>& arr) {
-        unordered_map<int,int>mp;
-        sort(arr.begin(),arr.end());
-        for(int i=0;i<arr.size();i++) mp[arr[i]]++;
-        for(int i=0;i<arr.size();i++)
-        {
-            if(mp[arr[i]] && mp[2*arr[i]]) 
-            {
-                mp[arr[i]]--;
-                mp[2*arr[i]]--;
+    bool canReorderDoubled(vector<int>& A) {
+        map<int,int>m;
+        int n=A.size();
+        for(int i=0;i<n;i++) m[A[i]]++;
+        int cnt=A.size();
+        map<int,int>::iterator it=m.begin();
+        while(it!=m.end()){
+            if(m[it->first]>0){
+                if(it->first!=0 && m[it->first*2]>0){
+                    int x=min(m[it->first],m[it->first*2]);
+                    cnt-=(x*2);
+                    m[it->first*2]-=x;
+                    m[it->first]-=x;
+                }
+                else if(it->first==0){
+                   cnt-=m[it->first];
+                   m[it->first]=0;
+                }
             }
+            it++;
         }
-        for(auto it:mp) if(it.second) return false;
-        return true;
+        return !cnt;
     }
 };
