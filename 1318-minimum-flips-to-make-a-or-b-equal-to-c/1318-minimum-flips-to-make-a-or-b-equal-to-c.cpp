009e3b5b86cf1d7f141344f5aac6d12e5d1215cd
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-10-26 16:07:17 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (11.04%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -2,22 +2,17 @@ class Solution {
 public:
     int minFlips(int a, int b, int c) {
         int ans=0;
-        while(a || b || c)
-        {
-            int a1=a&1,b1=b&1,c1=c&1;
-            if(c1==0)
-            {
-                if(a1+b1==0) ans+=0;
-                else if(a1+b1==1) ans+=1;
-                else if(a1+b1==2) ans+=2;
-            }
-            else
-            {
-                if(a1+b1==0) ans+=1;
-                else if(a1+b1==1) ans+=0;
-                else if(a1+b1==2) ans+=0;
-            }
-            a/=2,b/=2,c/=2;
+        while(a or b or c){
+            int bit1=a&1;
+            int bit2=b&1;
+            int bit3=c&1;
+            if(bit1==0 && bit2==0 && bit3==1) ans+=1;
+            if(bit1==0 && bit2==1 && bit3==0) ans+=1;
+            if(bit1==1 && bit2==0 && bit3==0) ans+=1;
+            if(bit1==1 && bit2==1 && bit3==0) ans+=2;
+            a>>=1;
+            b>>=1;
+            c>>=1;
         }
         return ans;
     }
