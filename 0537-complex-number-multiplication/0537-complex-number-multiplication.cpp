aa55e67d30f35c6cdc0e3b55cc1386cab1c2617d
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-10-28 11:57:06 +0530

    Time: 2 ms (52.59%), Space: 6.3 MB (53.41%) - LeetHub

--- 0537-complex-number-multiplication/0537-complex-number-multiplication.cpp
+++ 0537-complex-number-multiplication/0537-complex-number-multiplication.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    pair<int,int> parse(string s)
-    {
+    pair<int,int>solve(string s){
         int i=s.find('+');
+        int n=s.length();
         int real=stoi(s.substr(0,i));
-        int img=stoi(s.substr(i+1,s.length()-1-i-1));
-        pair<int,int>ans(real,img);
-        return ans;
+        int img=stoi(s.substr(i+1,n-i-2));
+        return {real,img};
     }
     string complexNumberMultiply(string num1, string num2) {
-        pair<int,int>a=parse(num1),b=parse(num2);
-        int reala=a.first,realb=b.first,imga=a.second,imgb=b.second;
-        return to_string(reala*realb-imga*imgb)+'+'+to_string(imga*realb+imgb*reala)+'i';
+        auto n1=solve(num1),n2=solve(num2);
+        int real1=n1.first,img1=n1.second,real2=n2.first,img2=n2.second;
+        string ans=to_string(real1*real2-img1*img2)+"+"+to_string(real1*img2+real2*img1)+"i";
+        return ans;
     }
 };
