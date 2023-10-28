a56c26f0a36f562343f4378abde423e2fd0917ca
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-10-28 18:19:35 +0530

    Time: 19 ms (60.31%), Space: 13.6 MB (51.69%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    bool valid(int l,int r,int c)
-    {
+    bool valid(int l,int r,int c){
         if(c<l || c>=r) return false;
         return true;
     }
-    int dfs(vector<vector<int>>&grid,int row,int col)
+    int dfs(vector<vector<int>>&grid,int row,int col,int m,int n)
     {
-        if(row==grid.size()) return col;
-        if(valid(0,grid[0].size(),col))
-        {
+        if(row==m) return col;
+        if(valid(0,n,col)){
             if(grid[row][col]==1)
-                if(valid(0,grid[0].size(),col+1) && grid[row][col+1]==1) return dfs(grid,row+1,col+1);
+                if(valid(0,grid[0].size(),col+1) && grid[row][col+1]==1) return dfs(grid,row+1,col+1,m,n);
             if(grid[row][col]==-1)
-                if(valid(0,grid[0].size(),col-1) && grid[row][col-1]==-1) return dfs(grid,row+1,col-1);
+                if(valid(0,grid[0].size(),col-1) && grid[row][col-1]==-1) return dfs(grid,row+1,col-1,m,n);
         }
         return -1;
     }
     vector<int> findBall(vector<vector<int>>& grid) {
         int m=grid.size(),n=grid[0].size();
         vector<int>ans(n,-1);
-        for(int i=0;i<n;i++) ans[i]=dfs(grid,0,i);
+        for(int i=0;i<n;i++) ans[i]=dfs(grid,0,i,m,n);
         return ans;
     }
 };
