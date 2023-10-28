574200882ff6574f7581b5ae0759c544d629d9c7
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-10-28 20:50:52 +0530

    Time: 4 ms (82.84%), Space: 8.6 MB (27.81%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>&board,int i,int j,int m,int n)
-    {
-        if(i<0 || j<0 || i>=m || j>=n || board[i][j]=='.') return;
-        board[i][j]='.';
-        dfs(board,i-1,j,m,n);
-        dfs(board,i+1,j,m,n);
-        dfs(board,i,j-1,m,n);
-        dfs(board,i,j+1,m,n);
+    void dfs(vector<vector<char>>&grid,int i,int j,int m,int n){
+        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]=='.') return;
+        grid[i][j]='.';
+        dfs(grid,i-1,j,m,n);
+        dfs(grid,i+1,j,m,n);
+        dfs(grid,i,j-1,m,n);
+        dfs(grid,i,j+1,m,n);
     }
-    int countBattleships(vector<vector<char>>& board) {
-        int m=board.size(),n=board[0].size(),ans=0;
-        for(int i=0;i<m;i++)
-            for(int j=0;j<n;j++) if(board[i][j]=='X') ans++,dfs(board,i,j,m,n);
+    int countBattleships(vector<vector<char>>& grid) {
+        int m=grid.size(),n=grid[0].size(),ans=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='X'){
+                    dfs(grid,i,j,m,n);
+                    ans++;
+                }
+            }
+        }
         return ans;
     }
 };
