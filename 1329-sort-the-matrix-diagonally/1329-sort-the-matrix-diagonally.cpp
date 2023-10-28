3a3d08341eee8521fadf1eca795b14135232ee17
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2023-10-28 19:25:52 +0530

    Time: 10 ms (35.54%), Space: 9.8 MB (19.59%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -3,12 +3,13 @@ public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& grid) {
         int m=grid.size(),n=grid[0].size();
         unordered_map<int,priority_queue<int,vector<int>,greater<int>>>mp;
-        for(int i=0;i<m;i++)
-            for(int j=0;j<n;j++) mp[i-j].push(grid[i][j]);
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                mp[i-j].push(grid[i][j]);
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 grid[i][j]=mp[i-j].top();
                 mp[i-j].pop();
             }
