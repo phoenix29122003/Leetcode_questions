6bfb10e4f1c39c4cd9ec74f4c467ebf720f6e2a8
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-02-15 00:11:28 +0530

    Time: 6 ms (8.55%), Space: 11.1 MB (14.06%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int>ans;
-        int i=0,j=0;
-        while(i<m && j<n)
-        {
-            if(nums1[i]<=nums2[j]) ans.push_back(nums1[i++]);
-            else ans.push_back(nums2[j++]);
+        vector<int>nums3(m+n,0);
+        int i=0,j=0,k=0;
+        while(i<m && j<n){
+            if(nums1[i]<=nums2[j]) nums3[k++]=nums1[i++];
+            else nums3[k++]=nums2[j++];
         }
-        while(i<m) ans.push_back(nums1[i++]);
-        while(j<n) ans.push_back(nums2[j++]);
-        for(int i=0;i<ans.size();i++) nums1[i]=ans[i];
+        while(i<m) nums3[k++]=nums1[i++];
+        while(j<n) nums3[k++]=nums2[j++];
+        k=0,i=0;
+        while(i<m+n && k<m+n) nums1[i++]=nums3[k++];
     }
 };
