971ee12394464451030c24e6df8718e633e36257
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-02-21 15:44:44 +0530

    Time: 29 ms (41.62%), Space: 12.1 MB (63.10%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int dp[1001][1001];
-    int solve(string &s,string &t,int i,int j,int m,int n){
+    int solve(string &s,string &p,int i,int j,int m,int n){
         if(i==m || j==n) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==t[j]) return dp[i][j]=1+solve(s,t,i+1,j+1,m,n);
-        else return dp[i][j]=max(solve(s,t,i+1,j,m,n),solve(s,t,i,j+1,m,n));
+        if(s[i]==p[j]) return dp[i][j]=1+solve(s,p,i+1,j+1,m,n);
+        else return dp[i][j]=max(solve(s,p,i+1,j,m,n),solve(s,p,i,j+1,m,n));
     }
-    int longestCommonSubsequence(string s, string t) {
+    int longestCommonSubsequence(string text1, string text2) {
         memset(dp,-1,sizeof(dp));
-        return solve(s,t,0,0,s.length(),t.length());
+        return solve(text1,text2,0,0,text1.size(),text2.size());
     }
 };
