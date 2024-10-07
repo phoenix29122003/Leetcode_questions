cad22d1437eeaf944642505a38c1e4551277bdf4
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-10-07 21:33:26 +0530

    Time: 3 ms (66.24%), Space: 7.5 MB (12.76%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,7 +1,13 @@
 class Solution {
 public:
+    int solve(int n,vector<int>&dp){
+        if(dp[n]!=-1) return dp[n];
+        if(n==0 || n==1) return n;
+        dp[n]=solve(n-1,dp)+solve(n-2,dp);
+        return dp[n];
+    }
     int fib(int n) {
-        if(n<=1) return n;
-        else return fib(n-1)+fib(n-2);
+        vector<int>dp(n+1,-1);
+        return solve(n,dp);
     }
 };
