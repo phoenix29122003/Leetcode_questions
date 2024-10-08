eb92e279d294a97211f4c53286b4daecc9be8365
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-10-08 20:17:33 +0530

    Time: 1050 ms (19.41%), Space: 12 MB (43.66%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    bool dfs(vector<vector<char>>& board,string word,int idx,int i,int j,int m,int n)
-    {
-        if(idx==word.length()) return true;
-        if(i<0 || j<0 || i>=m || j>=n) return false;
-        bool ans=false;
-        if(board[i][j]==word[idx])
-        {
-            board[i][j]='*';
-            ans=dfs(board,word,idx+1,i+1,j,m,n)||dfs(board,word,idx+1,i-1,j,m,n)||dfs(board,word,idx+1,i,j-1,m,n)||dfs(board,word,idx+1,i,j+1,m,n);
+    bool solve(vector<vector<char>>&board, string word, int idx,int i,int j,int m,int n,int o){
+        if(idx==word.size()) return true;
+        if(i>=m || j>=n || i<0 || j<0 || board[i][j]=='#') return false;
+        int ans=false;
+        if(board[i][j]==word[idx]){
+            board[i][j]='#';
+            ans=(solve(board,word,idx+1,i+1,j,m,n,o)||
+                   solve(board,word,idx+1,i-1,j,m,n,o)||
+                   solve(board,word,idx+1,i,j+1,m,n,o)||
+                   solve(board,word,idx+1,i,j-1,m,n,o));
             board[i][j]=word[idx];
         }
         return ans;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        int m=board.size(),n=board[0].size();
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(board[i][j]==word[0]) 
-                {
-                    if(dfs(board,word,0,i,j,m,n)) return true;
+        int m=board.size(),n=board[0].size(),o=word.size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]==word[0]){
+                    if(solve(board,word,0,i,j,m,n,o)) return true;
                 }
             }
         }
