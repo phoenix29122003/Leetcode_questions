bf5cb60e9ff5e43ab1c570fbc923b7f222941541
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-10-08 16:30:50 +0530

    Time: 162 ms (66.11%), Space: 102.1 MB (62.15%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
@@ -1,25 +1,31 @@
 class RandomizedSet {
 public:
     unordered_map<int,int>mp;
-    vector<int>vec;
-    RandomizedSet() {}
+    vector<int>nums;
+    RandomizedSet() {
+        
+    }
+    
     bool insert(int val) {
         if(mp.find(val)!=mp.end()) return false;
-        vec.push_back(val);
-        mp[val]=vec.size()-1;
+        nums.push_back(val);
+        mp[val]=nums.size()-1;
         return true;
     }
+    
     bool remove(int val) {
         if(mp.find(val)==mp.end()) return false;
-        int last=vec.back();
-        vec[mp[val]]=last;
-        mp[last]=mp[val];
-        vec.pop_back();
+        int lv=nums.back();
+        int idx=mp[val];
+        nums[idx]=lv;
+        nums.pop_back();
+        mp[lv]=idx;
         mp.erase(val);
         return true;
     }
+    
     int getRandom() {
-        int rand_idx=rand()%vec.size();
-        return vec[rand_idx];
+        int idx=rand()%nums.size();
+        return nums[idx];
     }
 };
