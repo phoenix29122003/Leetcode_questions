20091b9a8ef99a19c1d6481bdd44ea93fcb3b685
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-10-08 14:55:44 +0530

    Time: 55 ms (85.83%), Space: 27.5 MB (52.13%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>>ans;
         int n=nums.size();
+        vector<vector<int>>ans;
+        if(n==0) return ans;
         sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++)
-        {
-            long long x=0-long(nums[i]);
+        for(int i=0;i<n;i++){
+            long long target=0-(long long)nums[i];
             int j=i+1,k=n-1;
-            while(j<k)
-            {
-                if(nums[j]+nums[k]==x)
-                {
-                    ans.push_back({nums[i],nums[j++],nums[k--]});
+            while(j<k){
+                if(nums[j]+nums[k]==target){
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    j++,k--;
                     while(j<k && nums[j]==nums[j-1]) j++;
                     while(j<k && nums[k]==nums[k+1]) k--;
                 }
-                else if(nums[j]+nums[k]<x) j++;
+                else if(nums[j]+nums[k]<target) j++;
                 else k--;
             }
             while(i+1<n && nums[i]==nums[i+1]) i++;
