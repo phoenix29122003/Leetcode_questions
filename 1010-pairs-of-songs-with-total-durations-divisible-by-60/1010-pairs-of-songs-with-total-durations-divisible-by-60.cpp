65fbe6b9d076a5cdba95efdd34b466f7365980fd
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-10-09 21:27:13 +0530

    Time: 122 ms (5.22%), Space: 31.9 MB (37.43%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int numPairsDivisibleBy60(vector<int>& nums) {
-        int n=nums.size(),ans=0;
+        long long  ans=0,n=nums.size();
         unordered_map<int,int>mp;
-        for(int i=0;i<n;i++) nums[i]=nums[i]%60;
-        for(int i=0;i<n;i++)
-        {
-            if(mp.find((60-nums[i])%60)!=mp.end()) ans+=mp[(60-nums[i])%60];
-            mp[nums[i]%60]++;
+        for(int i=0;i<n;i++) nums[i]=nums[i]%60;;
+        for(int i=0;i<n;i++){
+            cout<<nums[i]<<" "<<endl;
+            if(mp.find(((60-nums[i])%60))!=mp.end()) ans+=mp[(60-nums[i])%60];
+            mp[nums[i]]++;
         }
         return ans;
     }
