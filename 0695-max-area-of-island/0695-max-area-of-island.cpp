0ff9a187c75a0a02646da112aa76b5bf8d7a81a5
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-10-10 17:58:55 +0530

    Time: 15 ms (50.10%), Space: 26.6 MB (98.33%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>>&grid,int i,int j,int m,int n){
+    int solve(vector<vector<int>>&grid,int i,int j,int m,int n){
         if(i<0 || j<0 || i>=m || j>=n || grid[i][j]==0) return 0;
         grid[i][j]=0;
-        return 1+dfs(grid,i-1,j,m,n)+dfs(grid,i+1,j,m,n)+dfs(grid,i,j-1,m,n)+dfs(grid,i,j+1,m,n);
+        return 1+solve(grid,i-1,j,m,n)+solve(grid,i+1,j,m,n)+solve(grid,i,j-1,m,n)+solve(grid,i,j+1,m,n);
     }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int m=grid.size(),n=grid[0].size(),ans=0;
-        for(int i=0;i<m;i++)
-            for(int j=0;j<n;j++)
-                if(grid[i][j]==1) ans=max(ans,dfs(grid,i,j,m,n));
+        int ans=0,m=grid.size(),n=grid[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]) ans=max(ans,solve(grid,i,j,m,n));
+            }
+        }
         return ans;
     }
 };
