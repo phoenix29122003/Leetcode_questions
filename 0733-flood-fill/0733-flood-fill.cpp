7e30eaab608cf40e28718b337ac561c244c0bcfa
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-10-14 16:55:10 +0530

    Time: 8 ms (47.14%), Space: 17 MB (65.49%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
-    void solve(vector<vector<int>>& grid,int i,int j,int color1,int m,int n,int color2){
-        if(i<0 || j<0 || i>=m || j>=n || grid[i][j]!=color1 || grid[i][j]==color2) return;
-        grid[i][j]=color2;
-        solve(grid,i-1,j,color1,m,n,color2);
-        solve(grid,i+1,j,color1,m,n,color2);
-        solve(grid,i,j-1,color1,m,n,color2);
-        solve(grid,i,j+1,color1,m,n,color2);
+    void solve(vector<vector<int>>&img,int i,int j,int prev_col,int new_col,int m,int n){
+        if(i<0 || j<0 || i>=m || j>=n || img[i][j]!=prev_col) return;
+        if(prev_col==new_col) return;
+        img[i][j]=new_col;
+        solve(img,i+1,j,prev_col,new_col,m,n);
+        solve(img,i-1,j,prev_col,new_col,m,n);
+        solve(img,i,j+1,prev_col,new_col,m,n);
+        solve(img,i,j-1,prev_col,new_col,m,n);
     }
-    vector<vector<int>> floodFill(vector<vector<int>>& grid, int sr, int sc, int color) {
-        solve(grid,sr,sc,grid[sr][sc],grid.size(),grid[0].size(),color);
-        return grid;
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int m=image.size(),n=image[0].size();
+        solve(image,sr,sc,image[sr][sc],color,m,n);
+        return image;
     }
 };
