99fa769626a759e91e962ab74a0b2f86e1d66022
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-10-15 21:25:26 +0530

    Time: 49 ms (28.24%), Space: 52.3 MB (10.28%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    int dp[13][10001];
-    int solve(vector<int>&nums,int idx,int target){
+    int solve(vector<int>&nums,int idx,int n,int target,vector<vector<int>>&dp){
         if(target==0) return 0;
-        if(idx>=nums.size() || target<0) return INT_MAX-1;
+        if(target<0) return 100000007;
+        if(idx==n){
+            return target==0?0:100000007;
+        }
         if(dp[idx][target]!=-1) return dp[idx][target];
-        int take=1+solve(nums,idx,target-nums[idx]);
-        int nottake=solve(nums,idx+1,target);
+        int take=1+solve(nums,idx,n,target-nums[idx],dp);
+        int nottake=solve(nums,idx+1,n,target,dp);
         return dp[idx][target]=min(take,nottake);
     }
     int coinChange(vector<int>& nums, int target) {
-        memset(dp,-1,sizeof(dp));
-        int ans=solve(nums,0,target);
-        return ans>=INT_MAX-1?-1:ans;
+        vector<vector<int>>dp(nums.size()+1,vector<int>(target+1,-1));
+        int ans=solve(nums,0,nums.size(),target,dp);
+        return ans>=100000007?-1:ans;
     }
 };
