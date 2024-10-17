6d5dfc3a4e45fa6cd2ad192ec2126857d83c2084
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-10-17 15:30:55 +0530

    Time: 2 ms (41.41%), Space: 7.9 MB (51.59%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    int dp[101][101];
-    int solve(int i,int j,int m,int n){
+    int solve(int i,int j,int m,int n,vector<vector<int>>&dp){
+        if(i>=m || j>=n) return 0;
         if(i==m-1 && j==n-1) return 1;
-        else if(i>=m || j>=n) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        return dp[i][j]=solve(i+1,j,m,n)+solve(i,j+1,m,n);
+        return dp[i][j]=solve(i+1,j,m,n,dp)+solve(i,j+1,m,n,dp);
     }
     int uniquePaths(int m, int n) {
-        memset(dp,-1,sizeof(dp));
-        return solve(0,0,m,n);
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        return solve(0,0,m,n,dp);
     }
 };
