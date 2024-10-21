15209c76b49f97ed16b41824d2385a1c2ff44021
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-10-21 13:28:09 +0530

    Time: 48 ms (98.63%), Space: 62.1 MB (57.31%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -4,6 +4,7 @@ public:
         if(ds.size()==k){
             ans.push_back(ds);
         }
+        if(ds.size()>k) return;
         for(int i=idx;i<=n;i++){
             ds.push_back(i);
             solve(i+1,n,k,ds,ans);
