64b43ce9d12471a0c60335c324f0ab9a22abfb45
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-10-22 18:27:15 +0530

    Time: 19 ms (93.19%), Space: 25.3 MB (35.50%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,13 +3,14 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>>ans;
         unordered_map<string,vector<string>>mp;
-        for(int i=0;i<strs.size();i++)
-        {
+        for(int i=0;i<strs.size();i++){
             string str=strs[i];
             sort(str.begin(),str.end());
             mp[str].push_back(strs[i]);
         }
-        for(auto it:mp) ans.push_back(it.second);
+        for(auto it:mp){
+            ans.push_back(it.second);
+        }
         return ans;
     }
 };
