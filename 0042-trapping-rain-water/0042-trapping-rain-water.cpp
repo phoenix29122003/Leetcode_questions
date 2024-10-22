2cb39fa19629d72db165102f1aab36808fef7504
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-10-22 10:24:33 +0530

    Time: 335 ms (5.10%), Space: 25.9 MB (30.56%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     int trap(vector<int>& nums) {
         int n=nums.size(),ans=0;
-        vector<int>lr(n+1,0),rl(n+1,0);
-        for(int i=1;i<=n;i++) lr[i]=max(lr[i-1],nums[i-1]);
-        for(int i=n-1;i>=0;i--) rl[i]=max(rl[i+1],nums[i]);
-        for(int i=1;i<=n;i++){
-            int miniwall=min(lr[i-1],rl[i]);
-            if(miniwall<=nums[i-1]) ans+=0;
-            else ans+=min(lr[i-1],rl[i])-nums[i-1];
+        vector<int>lm(n,0),rm(n,0);
+        for(int i=1;i<n;i++) lm[i]=max(lm[i-1],nums[i-1]);
+        for(int i=n-2;i>=0;i--) rm[i]=max(rm[i+1],nums[i+1]);
+        for(int i=0;i<n;i++) cout<<lm[i]<<" ";
+        cout<<endl;
+        for(int i=0;i<n;i++) cout<<rm[i]<<" ";
+        cout<<endl;
+        for(int i=0;i<n;i++){
+            int minh=min(lm[i],rm[i]);
+            if(minh-nums[i]>0) ans+=minh-nums[i];
         }
         return ans;
     }
