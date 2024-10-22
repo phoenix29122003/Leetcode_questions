329fcb6aaf6707cc0c218a5ecd84371841b2d4e0
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-10-22 14:56:48 +0530

    Time: 4 ms (51.29%), Space: 8.5 MB (55.45%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-    string addStrings(string s, string t) {
-        int i=s.length()-1,j=t.length()-1;
-        int carry=0;
+    string addStrings(string num1, string num2) {
         string ans="";
-        while(i>=0 || j>=0 || carry){
-            int x=i>=0?s[i--]-'0':0;
-            int y=j>=0?t[j--]-'0':0;
-            int sum=x+y+carry;
+        int m=num1.length(),n=num2.length(),i=m-1,j=n-1,carry=0;
+        while(i>=0 || j>=0){
+            int n1=0,n2=0;
+            if(i>=0) n1=num1[i]-'0';
+            if(j>=0) n2=num2[j]-'0';
+            int sum=n1+n2+carry;
             carry=sum/10;
-            ans+=sum%10+'0';
+            sum=sum%10;
+            ans+=sum+'0';
+            i--,j--;
         }
+        if(carry==1) ans+='1';
         reverse(ans.begin(),ans.end());
         return ans;
     }
