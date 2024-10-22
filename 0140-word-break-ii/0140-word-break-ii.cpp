709b880589bf219e9acf3092b4b60b6c6fe68594
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-10-22 11:04:23 +0530

    Time: 0 ms (100.00%), Space: 9.2 MB (63.78%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    void solve(string s,int idx,int n,set<string>&set,string ds,vector<string>&ans){
+    void solve(string &s,int idx,int n,unordered_set<string>&set,string ds,vector<string>&ans){
         if(idx==n){
             ds.pop_back();
             ans.push_back(ds);
             return;
         }
         for(int i=idx;i<n;i++){
-            string str=s.substr(idx,i-idx+1);
-            if(set.find(str)!=set.end()){
-                solve(s,i+1,n,set,ds+str+" ",ans);
+            if(set.find(s.substr(idx,i-idx+1))!=set.end()){
+                solve(s,i+1,n,set,ds+s.substr(idx,i-idx+1)+" ",ans);
             }
         }
     }
-    vector<string> wordBreak(string s, vector<string>& word) {
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
         vector<string>ans;
-        string ds="";
-        set<string>set(word.begin(),word.end());
-        solve(s,0,s.length(),set,ds,ans);
+        string str="";
+        unordered_set<string>set(wordDict.begin(),wordDict.end());
+        solve(s,0,s.length(),set,str,ans);
         return ans;
     }
 };
