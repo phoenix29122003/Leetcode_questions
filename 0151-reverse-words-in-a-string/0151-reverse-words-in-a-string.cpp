2dfc8e9d3471ed552f6515e7784e6ab2f91ffd47
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-10-23 20:05:55 +0530

    Time: 74 ms (5.14%), Space: 23.5 MB (15.24%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int n=s.size();
-        int i=0,j=0;
         string ans="";
-        while(i<n)
-        {
-            while(i<n && s[i]==' ') i++;
-            if(i>=n) break;
-            j=i;
-            while(j<n && s[j]!=' ') j++;
-            string str=s.substr(i,j-i);
-            if(ans=="") ans=str;
-            else ans=str+" "+ans;
-            i=j+1;
+        string curr="";
+        int n=s.length();
+        int j=0,i=0;
+        while(i<n && j<n){
+            if(s[j]==' '){
+                cout<<"curr: "<<curr<<endl;
+                if(ans.length()==0) ans=curr;
+                else if(curr.length()) ans=curr+" "+ans;
+                curr="";
+                i=j+1;
+                j++;
+            }
+            else{
+                curr+=s[j];
+                j++;
+            }
         }
+        if(curr.length() && ans.length()) ans=curr+" "+ans;
+        else if(curr.length() && ans.length()==0) ans=curr;
         return ans;
     }
 };
