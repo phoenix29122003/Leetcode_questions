68b815e9894171f3379c09f98341c5acbde5de7b
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-10-23 17:54:08 +0530

    Time: 0 ms (100.00%), Space: 15.2 MB (66.41%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
-    void solve(int o,int c,int n,string ds,vector<string>&ans)
-    {
-        if(ds.size()==2*n)
-        {
+    void solve(int o,int c,int n,string ds,vector<string>&ans){
+        if(c==n){
             ans.push_back(ds);
             return;
         }
-        if(o<n) solve(o+1,c,n,ds+'(',ans);
-        if(c<o) solve(o,c+1,n,ds+')',ans);
+        if(o<n) solve(o+1,c,n,ds+"(",ans);
+        if(c<o && c<n) solve(o,c+1,n,ds+")",ans); 
     }
     vector<string> generateParenthesis(int n) {
-        string ds="";
         vector<string>ans;
         solve(0,0,n,"",ans);
         return ans;
