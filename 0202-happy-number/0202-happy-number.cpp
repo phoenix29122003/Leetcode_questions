8880e3f15df3d3d5d3d263b8d95fe1604f25d9c6
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-10-24 15:23:11 +0530

    Time: 15 ms (20.40%), Space: 8.2 MB (38.15%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int digitSquareSum(int n){
+    int sumOfSquare(int n){
         int ans=0;
         while(n){
             ans+=(n%10)*(n%10);
@@ -9,12 +9,15 @@ public:
         return ans;
     }
     bool isHappy(int n) {
-        unordered_set<int>st;
-        while(1){
-            n=digitSquareSum(n);
-            if(n==1) return true;
-            if(st.find(n)!=st.end()) return false;
-            st.insert(n);
+        if(n==1) return true;
+        unordered_set<int>set;
+        int nextNum=n;
+        while(n!=1 && set.find(nextNum)==set.end()){
+            cout<<"nextNum: "<<nextNum<<endl;
+            set.insert(nextNum);
+            nextNum=sumOfSquare(nextNum);
+            if(nextNum==1) return true;
         }
+        return false;
     }
 };
