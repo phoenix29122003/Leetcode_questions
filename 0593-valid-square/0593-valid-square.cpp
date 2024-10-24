b50a50fc60b767e68818580e2e08f58cb188cd6b
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-10-24 19:16:33 +0530

    Time: 0 ms (100.00%), Space: 30.2 MB (5.62%) - LeetHub

--- 0593-valid-square/0593-valid-square.cpp
+++ 0593-valid-square/0593-valid-square.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
-    int caldis(int x1,int y1,int x2,int y2)
-    {
-        return abs(x1-x2)*abs(x1-x2)+abs(y1-y2)*abs(y1-y2);
+    int cal(vector<int>p1,vector<int>p2){
+        return (abs(p1[0]-p2[0]))*(abs(p1[0]-p2[0]))+(abs(p1[1]-p2[1]))*(abs(p1[1]-p2[1]));
     }
     bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
-        vector<vector<int>>points={p1,p2,p3,p4};
         unordered_set<int>set;
-        for(int i=0;i<points.size();i++)
-        {
-            for(int j=i+1;j<points.size();j++)
-            {
-                int dist=caldis(points[i][0],points[i][1],points[j][0],points[j][1]);
-                if(dist) set.insert(dist);
-                else return false;
-            }
-        }
-        return set.size()==2;
+        set.insert(cal(p1,p2));
+        set.insert(cal(p1,p4));
+        set.insert(cal(p1,p3));
+        set.insert(cal(p3,p2));
+        set.insert(cal(p3,p4));
+        set.insert(cal(p2,p4));
+        return !set.count(0) && set.size()==2;
     }
 };
