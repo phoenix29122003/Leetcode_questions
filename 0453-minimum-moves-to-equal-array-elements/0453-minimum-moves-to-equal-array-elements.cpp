901e48ff9c0a044e28467be70964fb26d3d1a026
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-10-24 15:09:42 +0530

    Time: 0 ms (100.00%), Space: 31.1 MB (41.02%) - LeetHub

--- 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.cpp
+++ 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int minMoves(vector<int>& nums) {
-        int mini=INT_MAX,ans=0;
-        for(auto it:nums) mini=min(mini,it);
-        for(auto it:nums) ans+=it-mini;
+        int ans=0,n=nums.size();
+        int mini=nums[0];
+        for(int i=0;i<n;i++) mini=min(mini,nums[i]);
+        for(int i=0;i<n;i++) ans+=nums[i]-mini;
         return ans;
     }
 };
