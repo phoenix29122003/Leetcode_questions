88ee763778b9dd0b6f7cc31cbb23611f5437d7d0
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-10-24 16:23:44 +0530

    Time: 3 ms (56.32%), Space: 9 MB (66.48%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int i=0,ans=0,n=s.length(),sign=1;
+        int i=0,n=s.length(),ans=0,sign=1;
         while(i<n && s[i]==' ') i++;
         if(s[i]=='-' || s[i]=='+') sign=s[i++]=='-'?-1:1;
-        while(i<n && ('0'<=s[i] && s[i]<='9')){
-            int digit=sign*(s[i]-'0');
-            if(sign==1 && (ans>INT_MAX/10 || (ans==INT_MAX/10 && digit>INT_MAX%10))) return INT_MAX;
-            if(sign==-1 && (ans<INT_MIN/10 || (ans==INT_MIN/10 && digit<INT_MIN%10))) return INT_MIN;
-            ans=ans*10+digit;
+        while(i<n){
+            if(s[i]>='0' && s[i]<='9'){
+                int digit=(s[i]-'0')*sign;
+                if(sign==1 && (ans>INT_MAX/10 || (ans==INT_MAX/10 && digit>INT_MAX%10))) return INT_MAX;
+                if(sign==-1 && (ans<INT_MIN/10 || (ans==INT_MIN/10 && digit<INT_MIN%10))) return INT_MIN;
+                ans=ans*10+digit;
+            }
+            else break;
             i++;
         }
         return ans;
