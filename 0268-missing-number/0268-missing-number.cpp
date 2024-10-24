3a78dcb253d66a73164e077c033a1c3ed8aded56
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-10-24 15:44:26 +0530

    Time: 8 ms (85.40%), Space: 21.4 MB (17.90%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        set<int>set;
-        int n=nums.size();
-        for(auto it:nums) set.insert(it);
-        for(int i=0;i<=n;i++) if(set.find(i)==set.end()) return i;
-        return 0;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++) if(nums[i]!=i) return i;
+        return nums.size();
     }
 };
