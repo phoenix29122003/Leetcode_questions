1fb7a5e3bd81644edfd53be81012dd68cc2f8cf7
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-10-27 18:49:24 +0530

    Time: 0 ms (100.00%), Space: 14.5 MB (34.47%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -6,13 +6,13 @@ public:
         queue<TreeNode*>q;
         q.push(root);
         while(q.size()){
-            int n=q.size();
-            for(int i=0;i<n;i++){
-                auto curr=q.front();
+            int sz=q.size();
+            for(int i=0;i<sz;i++){
+                TreeNode *curr=q.front();
                 q.pop();
-                if(i==0) ans.push_back(curr->val);
-                if(curr->right) q.push(curr->right);
+                if(i==sz-1) ans.push_back(curr->val);
                 if(curr->left) q.push(curr->left);
+                if(curr->right) q.push(curr->right);
             }
         }
         return ans;
