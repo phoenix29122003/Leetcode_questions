5b7572b943e78f7eec22215f9a9cd73a64ccec43
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-10-28 21:25:56 +0530

    Time: 29 ms (53.06%), Space: 20.2 MB (27.13%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     int minDeletions(string s) {
+        int n=s.length(),ans=0;
         unordered_map<char,int>mp;
         for(auto it:s) mp[it]++;
+        int max_freq=INT_MAX;
         priority_queue<pair<int,char>>pq;
         for(auto it:mp) pq.push({it.second,it.first});
-        int ans=0,prevfreq=INT_MAX;
         while(pq.size()){
-            char ch=pq.top().second;
-            int freq=pq.top().first;
-            if(freq>=prevfreq){
-                int cf=freq;
-                freq=prevfreq-1;
-                if(freq<0) ans+=cf;
-                else ans+=cf-freq;
+            char curr_ele=pq.top().second;
+            int curr_freq=pq.top().first;
+            if(max_freq==INT_MAX){
+                max_freq=curr_freq;
             }
-            prevfreq=freq;
+            else{
+                int desired_freq=max_freq-1;
+                if(curr_freq>=desired_freq){
+                    if(desired_freq<0) desired_freq=0;
+                    ans+=curr_freq-desired_freq;
+                    max_freq=desired_freq;
+                }
+                else{
+                    max_freq=curr_freq;
+                }
+            }
             pq.pop();
         }
         return ans;
