e30b22667592f725cb694ff6cc26d4b9898bf69f
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-10-30 18:55:36 +0530

    Time: 3 ms (45.32%), Space: 12.2 MB (65.84%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -6,15 +6,12 @@ public:
         dfs(grid,i-1,j,m,n,col),dfs(grid,i+1,j,m,n,col),dfs(grid,i,j-1,m,n,col),dfs(grid,i,j+1,m,n,col);
     }
     int closedIsland(vector<vector<int>>& grid) {
-        int m=grid.size(),n=grid[0].size();
+        int m=grid.size(),n=grid[0].size(),ans=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if((i*j==0 || i==m-1 || j==n-1)&&grid[i][j]==0){
-                    dfs(grid,i,j,m,n,2);
-                }
+                if(i==0 || j==0 || i==m-1 || j==n-1) dfs(grid,i,j,m,n,2);
             }
         }
-        int ans=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==0){
