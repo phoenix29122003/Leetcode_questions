01e70b4cdb4b0f7ab87ba666ef3a202685878918
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-10-30 21:27:33 +0530

    Time: 0 ms (100.00%), Space: 20.5 MB (5.05%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -3,6 +3,6 @@ public:
     ListNode* removeElements(ListNode* head, int val) {
         if(head==NULL) return NULL;
         head->next=removeElements(head->next,val);
-        return (head->val==val)?head->next:head;
+        return head->val==val?head->next:head;
     }
 };
