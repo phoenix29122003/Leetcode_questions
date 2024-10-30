4e07a125abdb844b535bbd8038d1961c729b5127
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-10-30 20:15:26 +0530

    Time: 107 ms (13.50%), Space: 18.5 MB (48.87%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1==NULL) return list2;
-        else if(list2==NULL) return list1;
-        else if(list1->val<=list2->val)
-        {
-            list1->next=mergeTwoLists(list1->next,list2);
-            return list1;
+        ListNode *dum=new ListNode(0);
+        ListNode *head=dum;
+        while(list1 && list2){
+            if(list1->val<=list2->val){
+                dum->next=list1;
+                list1=list1->next;
+                dum=dum->next;
+            }else{
+                dum->next=list2;
+                list2=list2->next;
+                dum=dum->next;
+            }
         }
-        else 
-        {
-            list2->next=mergeTwoLists(list1,list2->next);
-            return list2;
-        }
+        if(list1) dum->next=list1;
+        if(list2) dum->next=list2;
+        return head->next;
     }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         if(lists.size()==0) return NULL;
         ListNode *head=lists[0];
-        for(int i=1;i<lists.size();i++) head=mergeTwoLists(head,lists[i]);
+        for(int i=1;i<lists.size();i++){
+            head=mergeTwoLists(head,lists[i]);
+        }
         return head;
     }
 };
