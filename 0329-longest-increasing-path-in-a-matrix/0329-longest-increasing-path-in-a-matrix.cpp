7ff4c0bc99fee41a91f51c714468f67215a53c55
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-10-31 22:15:34 +0530

    Time: 12 ms (61.82%), Space: 20.2 MB (48.87%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    int dp[201][201];
-    int solve(vector<vector<int>>&grid,int i,int j,int m,int n,int prev){
-        if(i<0 || j<0 || i>=m || j>=n || grid[i][j]<=prev) return 0;
+    int solve(vector<vector<int>>&matrix,int i,int j,int m,int n,int prev,vector<vector<int>>&dp){
+        if(i<0 || j<0 || i>=m || j>=n || matrix[i][j]<=prev) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        int ele=grid[i][j];
-        return dp[i][j]=1+max({solve(grid,i-1,j,m,n,ele),solve(grid,i+1,j,m,n,ele),solve(grid,i,j-1,m,n,ele),solve(grid,i,j+1,m,n,ele)});
+        return dp[i][j]=1+max({solve(matrix,i-1,j,m,n,matrix[i][j],dp),
+                               solve(matrix,i+1,j,m,n,matrix[i][j],dp),
+                               solve(matrix,i,j-1,m,n,matrix[i][j],dp),
+                               solve(matrix,i,j+1,m,n,matrix[i][j],dp)});
     }
-    int longestIncreasingPath(vector<vector<int>>& grid) {
-        memset(dp,-1,sizeof(dp));
-        int m=grid.size(),n=grid[0].size(),ans=0;
-        for(int i=0;i<m;i++)
-            for(int j=0;j<n;j++)
-                ans=max(ans,solve(grid,i,j,m,n,-1));
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int m=matrix.size(),n=matrix[0].size(),ans=0;
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                ans=max(ans,solve(matrix,i,j,m,n,-1,dp));
+            }
+        }
         return ans;
     }
 };
