2175340da03c1cd8e8dc8971b84c2bdceca22204
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-11-01 13:29:13 +0530

    Time: 4 ms (20.03%), Space: 19.2 MB (8.46%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid,int i,int j,int m,int n,char ch){
-        if(i<0 || j<0 || i>=m || j>=n || grid[i][j]!='O') return;
+    void dfs(vector<vector<char>>&grid,int i,int j,int m,int n,char ch){
+        if(i<0 || j<0 || i==m || j==n || grid[i][j]!='O') return;
         grid[i][j]=ch;
-        dfs(grid,i-1,j,m,n,ch),dfs(grid,i+1,j,m,n,ch),dfs(grid,i,j-1,m,n,ch),dfs(grid,i,j+1,m,n,ch);
+        dfs(grid,i-1,j,m,n,ch);
+        dfs(grid,i+1,j,m,n,ch);
+        dfs(grid,i,j-1,m,n,ch);
+        dfs(grid,i,j+1,m,n,ch);
     }
     void solve(vector<vector<char>>& grid) {
         int m=grid.size(),n=grid[0].size();
-        for(int i=0;i<m;i++)
-            for(int j=0;j<n;j++) if((i==0 || i==m-1 || j==0 || j==n-1)&&grid[i][j]=='O') dfs(grid,i,j,m,n,'#');
-        for(int i=0;i<m;i++)
-            for(int j=0;j<n;j++) if(grid[i][j]=='O') dfs(grid,i,j,m,n,'X');
-        for(int i=0;i<m;i++)
-            for(int j=0;j<n;j++) if(grid[i][j]=='#') grid[i][j]='O';
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if((i==0 || j==0 || i==m-1 || j==n-1)&& grid[i][j]=='O'){
+                    dfs(grid,i,j,m,n,'#');
+                }
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='O') dfs(grid,i,j,m,n,'X');
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='#') grid[i][j]='O';
+            }
+        }
     }
 };
