dc897ee0bee3d381ab2076d3698551f63848c7a9
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-11-01 12:23:34 +0530

    Time: 71 ms (5.14%), Space: 20.7 MB (11.69%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-    void dfs(map<int,vector<int>>mp,int sr,vector<bool>&vis){
-        vis[sr]=true;
-        for(auto it:mp[sr]){
-            if(vis[it]==false) dfs(mp,it,vis);
+    void dfs(unordered_map<int,vector<int>>&graph,vector<bool>&visited,int node){
+        visited[node]=true;
+        for(auto nbr:graph[node]){
+            if(visited[nbr]==false) dfs(graph,visited,nbr);
         }
     }
-    int findCircleNum(vector<vector<int>>& graph) {
-        int n=graph.size(),ans=0;
-        map<int,vector<int>>mp;
-        for(int i=0;i<n;i++)
-            for(int j=0;j<n;j++)
-                if(graph[i][j]==1) mp[i].push_back(j);
-        for(auto it:mp){
-            cout<<it.first<<" ";
-            for(auto it2:it.second) cout<<it2<<" ";
+    int findCircleNum(vector<vector<int>>& grid) {
+        int n=grid.size(),ans=0;
+        unordered_map<int,vector<int>>graph;
+        vector<bool>visited(n,false);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i!=j && grid[i][j]==1) graph[i].push_back(j);
+            }
+        }
+        for(auto node:graph){
+            cout<<node.first<<": ";
+            for(auto it:graph[node.first]) cout<<it<<" ";
             cout<<endl;
         }
-        vector<bool>vis(n,false);
-        for(auto it:mp){
-            if(vis[it.first]==false){
+        for(int i=0;i<n;i++){
+            if(visited[i]==false){
                 ans++;
-                dfs(mp,it.first,vis);
+                dfs(graph,visited,i);
             }
         }
         return ans;
