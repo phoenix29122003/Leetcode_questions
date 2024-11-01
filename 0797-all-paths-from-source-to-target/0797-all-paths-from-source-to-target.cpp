24a3edb14fd42f69bc40d061fdc15ac3c0405e61
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-11-01 12:35:04 +0530

    Time: 15 ms (27.07%), Space: 17.7 MB (37.59%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
-    void solve(unordered_map<int,vector<int>>mp,int sr,int ed,vector<bool>vis,vector<int>ds,vector<vector<int>>&ans){
-        if(sr==ed){
-            ds.push_back(sr);
+    void solve(vector<vector<int>>& graph,int src,int dest,vector<int>ds,vector<vector<int>>&ans){
+        if(src==dest-1){
+            ds.push_back(src);
             ans.push_back(ds);
             return;
         }
-        ds.push_back(sr);
-        vis[sr]=true;
-        for(auto it:mp[sr])
-            if(vis[it]==false)
-                solve(mp,it,ed,vis,ds,ans);
+        ds.push_back(src);
+        for(auto it:graph[src]){
+            solve(graph,it,dest,ds,ans);
+        }
     }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        vector<int>ds;
-        vector<bool>vis(graph.size(),false);
         vector<vector<int>>ans;
-        unordered_map<int,vector<int>>mp;
-        for(int i=0;i<graph.size();i++)
-            for(int j=0;j<graph[i].size();j++) mp[i].push_back(graph[i][j]);
-        solve(mp,0,graph.size()-1,vis,ds,ans);
+        vector<int>ds;
+        solve(graph,0,graph.size(),ds,ans);
         return ans;
     }
 };
