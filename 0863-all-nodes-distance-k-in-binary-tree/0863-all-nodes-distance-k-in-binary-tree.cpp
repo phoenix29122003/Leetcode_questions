e4a11c6cdbcf46bfd8d275df323123ced7638c12
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-11-01 21:12:04 +0530

    Time: 3 ms (82.67%), Space: 16.5 MB (7.53%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    unordered_map<TreeNode*,TreeNode*>mp;
-    unordered_set<TreeNode*>set;
-    vector<int>ans;
-    void parentmapping(TreeNode *root,TreeNode *parent){
-        if(root==NULL) return;
-        mp[root]=parent;
-        parentmapping(root->left,root);
-        parentmapping(root->right,root);
-    }
-    void solve(TreeNode *root,TreeNode *node,int k){
-        if(node==NULL) return;
-        if(set.find(node)!=set.end()) return;
-        else set.insert(node);
+    void solve(TreeNode *root,TreeNode *target,int k,unordered_map<TreeNode*,TreeNode*>&mp,unordered_set<TreeNode*>&set,vector<int>&ans){
+        if(target==NULL) return;
+        if(set.find(target)!=set.end()) return;
+        else set.insert(target);
         if(k==0){
-            ans.push_back(node->val);
+            ans.push_back(target->val);
             return;
         }
-        solve(root,mp[node],k-1);
-        solve(root,node->left,k-1);
-        solve(root,node->right,k-1);
+        solve(root,mp[target],k-1,mp,set,ans);
+        solve(root,target->left,k-1,mp,set,ans);
+        solve(root,target->right,k-1,mp,set,ans);
+    }
+    void trace_parent(TreeNode *root,TreeNode *parent,unordered_map<TreeNode*,TreeNode*>&mp){
+        if(root==NULL) return;
+        mp[root]=parent;
+        trace_parent(root->left,root,mp);
+        trace_parent(root->right,root,mp);
     }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        parentmapping(root,NULL);
-        solve(root,target,k);
+        vector<int>ans;
+        unordered_map<TreeNode*,TreeNode*>mp;
+        unordered_set<TreeNode*>set;
+        trace_parent(root,NULL,mp);
+        solve(root,target,k,mp,set,ans);
         return ans;
     }
 };
