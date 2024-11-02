291115d15f27e2a01cdbeb3ba0e1925245ba5afc
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-11-02 12:39:35 +0530

    Time: 0 ms (100.00%), Space: 60.7 MB (55.53%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    TreeNode *first=NULL,*second=NULL,*prev=NULL;
-    void solve(TreeNode *root){
+    void solve(TreeNode *root,TreeNode *&prev,TreeNode *&nodeOne,TreeNode *&nodeTwo){
         if(root==NULL) return;
-        solve(root->left);
-        if(prev && prev->val>root->val){
-            if(first==NULL) first=prev;
-            second=root;
+        solve(root->left,prev,nodeOne,nodeTwo);
+        if(prev && root->val<prev->val){
+            if(nodeOne==NULL) nodeOne=prev;
+            nodeTwo=root;
         }
         prev=root;
-        solve(root->right);
+        solve(root->right,prev,nodeOne,nodeTwo);
     }
     void recoverTree(TreeNode* root) {
-        solve(root);
-        int dum=first->val;
-        first->val=second->val;
-        second->val=dum;
+        TreeNode *nodeOne=NULL,*nodeTwo=NULL,*prev=NULL;
+        solve(root,prev,nodeOne,nodeTwo);
+        if (nodeOne && nodeTwo) {
+            swap(nodeOne->val, nodeTwo->val);
+        }
     }
 };
