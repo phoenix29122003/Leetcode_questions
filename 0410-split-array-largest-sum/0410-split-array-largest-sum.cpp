1cf4db0c7e80bcd7dca285ebe1817f2705387d37
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-11-04 21:01:47 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (72.98%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    bool solve(vector<int>nums,int m,int k)
-    {
-        int cnt=1,curr_sum=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(curr_sum+nums[i]<=m) curr_sum+=nums[i];
-            else curr_sum=nums[i],cnt++;
+    bool solve(vector<int>&nums,int mid,int k,int n){
+        int cnt=1,sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            if(sum>mid) cnt++,sum=nums[i];
         }
         return cnt<=k;
     }
     int splitArray(vector<int>& nums, int k) {
-        int l=0,r=0,ans=INT_MAX;
-        for(auto it:nums) l=max(l,it),r+=it;
-        while(l<=r)
-        {
-            int m=(l+r)/2;
-            if(solve(nums,m,k)) ans=m,r=m-1;
-            else l=m+1;
+        int n=nums.size(),ans=-1;
+        int mini=nums[0],sum=0;
+        for(int i=0;i<n;i++) mini=max(mini,nums[i]),sum+=nums[i];
+        int i=mini,j=sum;
+        while(i<=j){
+            int mid=(i+j)/2;
+            if(solve(nums,mid,k,n)) ans=mid,j=mid-1;
+            else i=mid+1;
         }
         return ans;
     }
