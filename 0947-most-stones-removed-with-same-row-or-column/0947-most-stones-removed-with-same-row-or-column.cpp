d20d3e7b5b1928f29f8a97305c75c8064421da1b
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-11-18 20:21:08 +0530

    Time: 80 ms (23.41%), Space: 18.2 MB (83.87%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>>&stones,int index,vector<bool>&visited,int&n){
-        visited[index]=true;
-        int result=0;
-        for(int i=0;i<n;i++)
-            if(!visited[i]&&(stones[i][0]==stones[index][0]||stones[i][1]==stones[index][1]))
-                result+=(dfs(stones,i,visited,n)+1);
-        return result;
-    }
-    int removeStones(vector<vector<int>>&stones) {
-        int n=stones.size();
-        vector<bool>visited(n,0);
-        int result=0;
-        for(int i=0;i<n;i++){
-            if(visited[i]){continue;}
-            result+=dfs(stones,i,visited,n);
+    void dfs(int currIdx,vector<vector<int>>& stones,vector<int>&visited){
+        visited[currIdx]=1;
+        for(int i=0;i<stones.size();i++){
+            if(visited[i]==0 && (stones[i][0]==stones[currIdx][0] || stones[i][1]==stones[currIdx][1])) dfs(i,stones,visited);
         }
-        return result;
+    }
+    int removeStones(vector<vector<int>>& stones) {
+        int n=stones.size();
+        vector<int>visited(n,0);
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(visited[i]==0){
+                cnt++;
+                dfs(i,stones,visited);
+            }
+        }
+        return n-cnt;
     }
 };
