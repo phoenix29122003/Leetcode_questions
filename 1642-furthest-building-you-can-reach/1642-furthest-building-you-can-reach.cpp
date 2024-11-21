9a768d530502b5f0805606bb3bbfdf0800155998
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-11-21 20:09:55 +0530

    Time: 16 ms (47.14%), Space: 58 MB (69.63%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
-        priority_queue<int>pq;
-        int n=heights.size();
-        for(int i=0;i<n-1;i++)
-        {
-            if(heights[i]>=heights[i+1]) continue;
-            bricks-=heights[i+1]-heights[i];
-            pq.push(heights[i+1]-heights[i]);
-            if(bricks<0)
-            {
-                bricks+=pq.top();
-                pq.pop();
-                if(ladders>0) ladders--;
-                else return i;
+        priority_queue<int,vector<int>,greater<int>>pq;
+        int n=heights.size(),i=0;
+        while(i<n-1){
+            int diff=heights[i+1]-heights[i];
+            if(diff>0){
+                pq.push(diff);
+                if(pq.size()>ladders){
+                    int required_bricks=pq.top();
+                    pq.pop();
+                    bricks-=required_bricks;
+                    if(bricks<0) return i;
+                }
             }
+            i++;
         }
-        return n-1;
+        return i;
     }
 };
