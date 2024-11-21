7e519591dc55a379009230f79b22761a79d4a6e4
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-11-21 19:23:48 +0530

    Time: 1 ms (40.65%), Space: 8.2 MB (75.71%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -3,30 +3,23 @@ public:
     string reorganizeString(string s) {
         int n=s.length();
         unordered_map<char,int>mp;
-        for(auto it:s) mp[it]++;
-        int maxfreq=0;
-        for(auto it:mp) maxfreq=max(maxfreq,it.second);
-        if(maxfreq>(n+1)/2) return "";
+        for(int i=0;i<n;i++) mp[s[i]]++;
         priority_queue<pair<int,char>>pq;
-        for(auto it:mp) pq.push({it.second,it.first});
+        for(auto it:mp){
+            pq.push({it.second,it.first});
+        }
+        if(pq.top().first>(n+1)/2) return "";
         int i=0;
-        vector<char>ans(n);
         while(pq.size()){
-            auto x=pq.top();
+            int freq=pq.top().first;
+            char ch=pq.top().second;
             pq.pop();
-            char ch=x.second;
-            int freq=x.first;
             while(freq--){
-                if(i==n || i>n) i=1;
-                ans[i]=ch;
+                s[i]=ch;
                 i+=2;
+                if(i>=n) i=1;
             }
         }
-        string res="";
-        int j=0;
-        while(j<n){
-            res+=ans[j++];
-        }
-        return res;
+        return s;
     }
 };
